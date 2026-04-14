a576bd2e435c31853be9c3fd759ce19a97cf8c2c
sabuguinni <hugomartins@transpersonalinternational.com>
2026-04-14 06:32:31 -0100

    Add narrative types implementation file

---
+++ Source/TranspersonalGame/Narrative/NarrativeTypes.cpp
@@ -0,0 +1,5 @@
+#include "NarrativeTypes.h"
+
+// Implementation file for narrative types
+// Most functionality is handled by the structs themselves
+// This file exists to satisfy UE5 compilation requirements
