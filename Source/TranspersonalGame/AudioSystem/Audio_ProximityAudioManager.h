10f3f332e0eb7da75feed4080f49fcf37ee3cd37
sabuguinni <hugomartins@transpersonalinternational.com>
2026-05-14 13:52:23 +0000

    Add Audio_ProximityAudioManager header - proximity-based environmental and dinosaur audio system

--- Source/TranspersonalGame/AudioSystem/Audio_ProximityAudioManager.h
+++ Source/TranspersonalGame/AudioSystem/Audio_ProximityAudioManager.h
@@ -1,138 +1,184 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GameFramework/Actor.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "Engine/World.h"
 #include "Components/AudioComponent.h"
-#include "Engine/DataTable.h"
 #include "Sound/SoundCue.h"
+#include "MetasoundSource.h"
+#include "SharedTypes.h"
 #include "Audio_ProximityAudioManager.generated.h"
 
+class ATranspersonalCharacter;
+class AActor;
+
 USTRUCT(BlueprintType)
-struct TRANSPERSONALGAME_API FAudio_ProximityTrigger
+struct TRANSPERSONALGAME_API FAudio_ProximityAudioZone
 {
     GENERATED_BODY()
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Proximity Audio")
-    float TriggerDistance = 1000.0f;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Audio Zone")
+    FVector ZoneCenter = FVector::ZeroVector;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Proximity Audio")
-    TSoftObjectPtr<USoundCue> ProximitySoundCue;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Audio Zone")
+    float InnerRadius = 500.0f;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Proximity Audio")
-    float VolumeMultiplier = 1.0f;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Audio Zone")
+    float OuterRadius = 1000.0f;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Proximity Audio")
-    bool bLooping = true;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Audio Zone")
+    TSoftObjectPtr<USoundBase> AmbientSound;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Proximity Audio")
-    FString TriggerTag;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Audio Zone")
+    TSoftObjectPtr<UMetaSoundSource> MetaSoundAsset;
 
-    FAudio_ProximityTrigger()
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Audio Zone")
+    float BaseVolume = 0.7f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Audio Zone")
+    bool bIsActive = true;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Audio Zone")
+    EBiomeType BiomeType = EBiomeType::Forest;
+
+    FAudio_ProximityAudioZone()
     {
-        TriggerDistance = 1000.0f;
-        VolumeMultiplier = 1.0f;
-        bLooping = true;
-        TriggerTag = TEXT("Default");
+        ZoneCenter = FVector::ZeroVector;
+        InnerRadius = 500.0f;
+        OuterRadius = 1000.0f;
+        BaseVolume = 0.7f;
+        bIsActive = true;
+        BiomeType = EBiomeType::Forest;
     }
 };
 
-UCLASS(BlueprintType, Blueprintable)
-class TRANSPERSONALGAME_API AAudio_ProximityAudioManager : public AActor
+USTRUCT(BlueprintType)
+struct TRANSPERSONALGAME_API FAudio_DinosaurAudioProfile
+{
+    GENERATED_BODY()
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dinosaur Audio")
+    EDinosaurSpecies Species = EDinosaurSpecies::TRex;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dinosaur Audio")
+    TSoftObjectPtr<USoundBase> IdleSound;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dinosaur Audio")
+    TSoftObjectPtr<USoundBase> MovementSound;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dinosaur Audio")
+    TSoftObjectPtr<USoundBase> AggressiveSound;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dinosaur Audio")
+    float FootstepInterval = 2.0f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dinosaur Audio")
+    float ProximityThreatDistance = 2000.0f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dinosaur Audio")
+    float VolumeMultiplier = 1.0f;
+
+    FAudio_DinosaurAudioProfile()
+    {
+        Species = EDinosaurSpecies::TRex;
+        FootstepInterval = 2.0f;
+        ProximityThreatDistance = 2000.0f;
+        VolumeMultiplier = 1.0f;
+    }
+};
+
+UCLASS(BlueprintType)
+class TRANSPERSONALGAME_API UAudio_ProximityAudioManager : public UGameInstanceSubsystem
 {
     GENERATED_BODY()
 
 public:
-    AAudio_ProximityAudioManager();
+    UAudio_ProximityAudioManager();
+
+    // USubsystem interface
+    virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+    virtual void Deinitialize() override;
+
+    // Core proximity audio functions
+    UFUNCTION(BlueprintCallable, Category = "Proximity Audio")
+    void RegisterAudioZone(const FAudio_ProximityAudioZone& AudioZone);
+
+    UFUNCTION(BlueprintCallable, Category = "Proximity Audio")
+    void UnregisterAudioZone(FVector ZoneCenter, float Tolerance = 50.0f);
+
+    UFUNCTION(BlueprintCallable, Category = "Proximity Audio")
+    void UpdatePlayerProximity(FVector PlayerLocation);
+
+    UFUNCTION(BlueprintCallable, Category = "Proximity Audio")
+    void RegisterDinosaurActor(AActor* DinosaurActor, EDinosaurSpecies Species);
+
+    UFUNCTION(BlueprintCallable, Category = "Proximity Audio")
+    void UpdateDinosaurProximity();
+
+    // Environmental audio control
+    UFUNCTION(BlueprintCallable, Category = "Environmental Audio")
+    void SetGlobalAudioMood(EAudioMood NewMood);
+
+    UFUNCTION(BlueprintCallable, Category = "Environmental Audio")
+    void PlayNarrativeAudio(const FString& AudioURL, FVector WorldLocation = FVector::ZeroVector);
+
+    UFUNCTION(BlueprintCallable, Category = "Environmental Audio")
+    void StopAllProximityAudio();
+
+    // Dinosaur-specific audio
+    UFUNCTION(BlueprintCallable, Category = "Dinosaur Audio")
+    void TriggerDinosaurFootstep(AActor* DinosaurActor, float IntensityMultiplier = 1.0f);
+
+    UFUNCTION(BlueprintCallable, Category = "Dinosaur Audio")
+    void PlayDinosaurThreatSound(EDinosaurSpecies Species, FVector Location);
+
+    // Debug and utility
+    UFUNCTION(BlueprintCallable, Category = "Audio Debug", CallInEditor)
+    void DebugPrintAudioZones();
+
+    UFUNCTION(BlueprintCallable, Category = "Audio Debug")
+    int32 GetActiveAudioZoneCount() const;
 
 protected:
-    virtual void BeginPlay() override;
-
-public:
-    virtual void Tick(float DeltaTime) override;
-
-    // Proximity audio configuration
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Audio Configuration")
-    TArray<FAudio_ProximityTrigger> ProximityTriggers;
-
-    // Player reference for distance calculations
-    UPROPERTY(BlueprintReadOnly, Category = "Audio System")
-    class APawn* PlayerPawn;
-
-    // Audio components for different proximity zones
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Audio Components")
-    class UAudioComponent* TRexProximityAudio;
+    // Audio zone management
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Audio Zones")
+    TArray<FAudio_ProximityAudioZone> RegisteredAudioZones;
 
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Audio Components")
-    class UAudioComponent* RaptorPackAudio;
+    TArray<UAudioComponent*> ActiveAudioComponents;
 
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Audio Components")
-    class UAudioComponent* AmbientForestAudio;
+    // Dinosaur audio tracking
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Dinosaur Audio")
+    TMap<AActor*, FAudio_DinosaurAudioProfile> DinosaurAudioMap;
 
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Audio Components")
-    class UAudioComponent* FootstepAudio;
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Audio State")
+    FVector LastPlayerLocation = FVector::ZeroVector;
 
-    // T-Rex specific proximity settings
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "T-Rex Audio")
-    float TRexProximityDistance = 2000.0f;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Audio Settings")
+    EAudioMood CurrentAudioMood = EAudioMood::Neutral;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "T-Rex Audio")
-    float TRexFootstepInterval = 3.0f;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Audio Settings")
+    float GlobalVolumeMultiplier = 1.0f;
 
-    // Raptor pack proximity settings
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Raptor Audio")
-    float RaptorPackDistance = 800.0f;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Raptor Audio")
-    int32 MinRaptorPackSize = 3;
-
-    // Audio state tracking
-    UPROPERTY(BlueprintReadOnly, Category = "Audio State")
-    bool bTRexNearby = false;
-
-    UPROPERTY(BlueprintReadOnly, Category = "Audio State")
-    bool bRaptorPackNearby = false;
-
-    UPROPERTY(BlueprintReadOnly, Category = "Audio State")
-    float LastFootstepTime = 0.0f;
-
-    // Audio management functions
-    UFUNCTION(BlueprintCallable, Category = "Audio Management")
-    void UpdateProximityAudio();
-
-    UFUNCTION(BlueprintCallable, Category = "Audio Management")
-    void TriggerTRexProximityAudio(bool bEnable, float Distance);
-
-    UFUNCTION(BlueprintCallable, Category = "Audio Management")
-    void TriggerRaptorPackAudio(bool bEnable, int32 PackSize);
-
-    UFUNCTION(BlueprintCallable, Category = "Audio Management")
-    void PlayFootstepAudio(FVector Location, bool bIsLarge = false);
-
-    UFUNCTION(BlueprintCallable, Category = "Audio Management")
-    void UpdateAmbientAudio(const FString& BiomeType);
-
-    // Dinosaur detection functions
-    UFUNCTION(BlueprintCallable, Category = "Dinosaur Detection")
-    TArray<AActor*> GetNearbyDinosaurs(float SearchRadius);
-
-    UFUNCTION(BlueprintCallable, Category = "Dinosaur Detection")
-    bool IsTRexNearby(float& OutDistance);
-
-    UFUNCTION(BlueprintCallable, Category = "Dinosaur Detection")
-    bool IsRaptorPackNearby(int32& OutPackSize, float& OutDistance);
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Audio Settings")
+    float ProximityUpdateInterval = 0.1f;
 
 private:
     // Internal audio management
-    void InitializeAudioComponents();
-    void UpdateTRexAudio();
-    void UpdateRaptorAudio();
-    void UpdateAmbientAudio();
+    void UpdateAudioZoneVolumes(FVector PlayerLocation);
+    void CleanupInactiveAudioComponents();
+    UAudioComponent* CreateAudioComponentForZone(const FAudio_ProximityAudioZone& AudioZone);
+    float CalculateProximityVolume(FVector PlayerLocation, const FAudio_ProximityAudioZone& AudioZone);
     
-    // Audio fade and transition functions
-    void FadeAudioComponent(UAudioComponent* AudioComp, float TargetVolume, float FadeTime);
-    void CrossfadeAudio(UAudioComponent* FromComp, UAudioComponent* ToComp, float FadeTime);
+    // Dinosaur audio helpers
+    void ProcessDinosaurProximityAudio(AActor* DinosaurActor, const FAudio_DinosaurAudioProfile& AudioProfile);
+    float CalculateDinosaurThreatLevel(FVector PlayerLocation, FVector DinosaurLocation, float ThreatDistance);
 
-    // Distance calculation helpers
-    float GetDistanceToPlayer(const FVector& Location);
-    bool IsActorInRange(AActor* Actor, float Range);
+    // Timer handles
+    FTimerHandle ProximityUpdateTimer;
+    FTimerHandle DinosaurAudioTimer;
+
+    // Audio component pool for optimization
+    TArray<UAudioComponent*> PooledAudioComponents;
+    int32 MaxPooledComponents = 20;
 };
