5cb92a9592c81d40da69f7b4f93f16627f3210ef
sabuguinni <hugomartins@transpersonalinternational.com>
2026-05-22 07:51:38 +0000

    Add DialogueTrigger actor for location-based narrative events

--- Source/TranspersonalGame/Narrative/DialogueTrigger.h
+++ Source/TranspersonalGame/Narrative/DialogueTrigger.h
@@ -1,19 +1,13 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Engine/TriggerBox.h"
+#include "GameFramework/Actor.h"
+#include "Components/BoxComponent.h"
 #include "Components/StaticMeshComponent.h"
+#include "Engine/TriggerBox.h"
 #include "NarrativeManager.h"
+#include "../SharedTypes.h"
 #include "DialogueTrigger.generated.h"
-
-UENUM(BlueprintType)
-enum class ENarr_TriggerType : uint8
-{
-    OnEnter     UMETA(DisplayName = "On Enter"),
-    OnExit      UMETA(DisplayName = "On Exit"),
-    OnStay      UMETA(DisplayName = "On Stay"),
-    OnInteract  UMETA(DisplayName = "On Interact")
-};
 
 UCLASS(BlueprintType, Blueprintable)
 class TRANSPERSONALGAME_API ADialogueTrigger : public ATriggerBox
@@ -31,50 +25,28 @@ protected:
                        UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, 
                        bool bFromSweep, const FHitResult& SweepResult);
 
-    UFUNCTION()
-    void OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, 
-                     UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Narrative")
+    FString EventID;
 
-public:
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")
-    FString DialogueID;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Narrative")
+    bool bTriggerOnce;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")
-    FString StoryBeatID;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Narrative")
+    bool bRequirePlayerCharacter;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")
-    ENarr_TriggerType TriggerType;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Narrative")
+    float CooldownTime;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")
-    bool bOneTimeOnly;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")
-    bool bRequiresPlayerOnly;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")
-    float DelayBeforeTrigger;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")
-    TArray<FString> RequiredConditions;
-
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
-    UStaticMeshComponent* VisualIndicator;
-
-protected:
-    UPROPERTY(BlueprintReadOnly, Category = "Dialogue")
-    bool bHasTriggered;
-
-    UPROPERTY(BlueprintReadOnly, Category = "Dialogue")
-    UNarrativeManager* NarrativeManager;
-
-    UPROPERTY(BlueprintReadOnly, Category = "Dialogue")
-    FTimerHandle DelayTimerHandle;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Narrative")
+    TArray<FNarr_DialogueEntry> CustomDialogues;
 
 private:
-    void ExecuteTrigger(AActor* TriggeringActor);
-    void DelayedTriggerExecution();
-    bool CheckTriggerConditions(AActor* TriggeringActor) const;
-    void SetupVisualIndicator();
-};
+    UPROPERTY()
+    UNarrativeManager* NarrativeManager;
 
-#include "DialogueTrigger.generated.h"
+    bool bHasTriggered;
+    float LastTriggerTime;
+
+    void InitializeNarrativeManager();
+    bool CanTrigger() const;
+};
