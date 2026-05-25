c7313fbe629605deef567c7ed0561bcdb51ff373
sabuguinni <hugomartins@transpersonalinternational.com>
2026-05-25 10:40:52 +0000

    Add VFX Impact Manager system for blood, dust, and environmental effects

--- Source/TranspersonalGame/VFX/VFX_ImpactManager.h
+++ Source/TranspersonalGame/VFX/VFX_ImpactManager.h
@@ -1,148 +1,79 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GameFramework/Actor.h"
-#include "Components/StaticMeshComponent.h"
-#include "NiagaraComponent.h"
-#include "NiagaraSystem.h"
+#include "Components/ActorComponent.h"
 #include "Engine/Engine.h"
-#include "SharedTypes.h"
+#include "Particles/ParticleSystemComponent.h"
 #include "VFX_ImpactManager.generated.h"
 
 UENUM(BlueprintType)
 enum class EVFX_ImpactType : uint8
 {
-    DinosaurFootstep    UMETA(DisplayName = "Dinosaur Footstep"),
-    WeaponHit          UMETA(DisplayName = "Weapon Hit"),
-    BloodSplatter      UMETA(DisplayName = "Blood Splatter"),
-    RockCrush          UMETA(DisplayName = "Rock Crush"),
-    WaterSplash        UMETA(DisplayName = "Water Splash")
-};
-
-UENUM(BlueprintType)
-enum class EVFX_SurfaceType : uint8
-{
-    Dirt               UMETA(DisplayName = "Dirt"),
-    Stone              UMETA(DisplayName = "Stone"),
-    Grass              UMETA(DisplayName = "Grass"),
-    Water              UMETA(DisplayName = "Water"),
-    Sand               UMETA(DisplayName = "Sand"),
-    Mud                UMETA(DisplayName = "Mud")
+    Blood       UMETA(DisplayName = "Blood"),
+    Dust        UMETA(DisplayName = "Dust"),
+    Rock        UMETA(DisplayName = "Rock"),
+    Water       UMETA(DisplayName = "Water"),
+    Mud         UMETA(DisplayName = "Mud")
 };
 
 USTRUCT(BlueprintType)
-struct FVFX_ImpactSettings
+struct FVFX_ImpactData
 {
     GENERATED_BODY()
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Impact VFX")
-    float DustIntensity = 1.0f;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "VFX")
+    class UParticleSystem* ParticleEffect;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Impact VFX")
-    float ParticleLifetime = 3.0f;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "VFX")
+    FVector Scale;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Impact VFX")
-    FVector ParticleVelocity = FVector(0, 0, 100);
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "VFX")
+    float Duration;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Impact VFX")
-    FLinearColor ParticleColor = FLinearColor::White;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Impact VFX")
-    int32 ParticleCount = 50;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Impact VFX")
-    float ScreenShakeIntensity = 0.5f;
-
-    FVFX_ImpactSettings()
+    FVFX_ImpactData()
     {
-        DustIntensity = 1.0f;
-        ParticleLifetime = 3.0f;
-        ParticleVelocity = FVector(0, 0, 100);
-        ParticleColor = FLinearColor::White;
-        ParticleCount = 50;
-        ScreenShakeIntensity = 0.5f;
+        ParticleEffect = nullptr;
+        Scale = FVector(1.0f);
+        Duration = 2.0f;
     }
 };
 
-UCLASS(BlueprintType, Blueprintable)
-class TRANSPERSONALGAME_API AVFX_ImpactManager : public AActor
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class TRANSPERSONALGAME_API UVFX_ImpactManager : public UActorComponent
 {
     GENERATED_BODY()
 
 public:
-    AVFX_ImpactManager();
+    UVFX_ImpactManager();
 
 protected:
     virtual void BeginPlay() override;
 
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
-    USceneComponent* RootSceneComponent;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "VFX Impact")
+    TMap<EVFX_ImpactType, FVFX_ImpactData> ImpactEffects;
 
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "VFX Components")
-    UNiagaraComponent* DustParticleComponent;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "VFX Impact")
+    float DefaultEffectDuration;
 
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "VFX Components")
-    UNiagaraComponent* BloodParticleComponent;
-
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "VFX Components")
-    UNiagaraComponent* DebrisParticleComponent;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "VFX Settings")
-    TMap<EVFX_SurfaceType, FVFX_ImpactSettings> SurfaceSettings;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "VFX Settings")
-    TMap<EVFX_ImpactType, FVFX_ImpactSettings> ImpactTypeSettings;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Niagara Systems")
-    UNiagaraSystem* DustExplosionSystem;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Niagara Systems")
-    UNiagaraSystem* BloodSplatterSystem;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Niagara Systems")
-    UNiagaraSystem* DebrisSystem;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Niagara Systems")
-    UNiagaraSystem* WaterSplashSystem;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "VFX Impact")
+    bool bAutoCleanupEffects;
 
 public:
-    virtual void Tick(float DeltaTime) override;
+    UFUNCTION(BlueprintCallable, Category = "VFX Impact")
+    void SpawnImpactEffect(EVFX_ImpactType ImpactType, FVector Location, FRotator Rotation = FRotator::ZeroRotator);
 
     UFUNCTION(BlueprintCallable, Category = "VFX Impact")
-    void TriggerImpact(EVFX_ImpactType ImpactType, EVFX_SurfaceType SurfaceType, FVector Location, FVector Normal = FVector::UpVector);
+    void SpawnBloodSplatter(FVector Location, FVector ImpactDirection, float Intensity = 1.0f);
 
     UFUNCTION(BlueprintCallable, Category = "VFX Impact")
-    void TriggerDinosaurFootstep(FVector Location, float DinosaurSize = 1.0f, EVFX_SurfaceType SurfaceType = EVFX_SurfaceType::Dirt);
+    void SpawnDustCloud(FVector Location, float Size = 1.0f);
 
     UFUNCTION(BlueprintCallable, Category = "VFX Impact")
-    void TriggerWeaponImpact(FVector Location, FVector HitDirection, EVFX_SurfaceType SurfaceType = EVFX_SurfaceType::Stone);
-
-    UFUNCTION(BlueprintCallable, Category = "VFX Impact")
-    void TriggerBloodEffect(FVector Location, FVector BloodDirection, float BloodAmount = 1.0f);
-
-    UFUNCTION(BlueprintCallable, Category = "VFX Impact")
-    void TriggerEnvironmentalEffect(FVector Location, EVFX_SurfaceType SurfaceType);
-
-    UFUNCTION(BlueprintCallable, Category = "VFX Settings")
-    void SetImpactSettings(EVFX_ImpactType ImpactType, const FVFX_ImpactSettings& Settings);
-
-    UFUNCTION(BlueprintCallable, Category = "VFX Settings")
-    void SetSurfaceSettings(EVFX_SurfaceType SurfaceType, const FVFX_ImpactSettings& Settings);
-
-    UFUNCTION(BlueprintCallable, Category = "VFX Management")
-    void ClearAllActiveEffects();
-
-    UFUNCTION(BlueprintCallable, Category = "VFX Management")
-    void SetEffectIntensityMultiplier(float Multiplier);
+    void InitializeDefaultEffects();
 
 private:
-    void InitializeDefaultSettings();
-    void PlayParticleEffect(UNiagaraSystem* System, FVector Location, const FVFX_ImpactSettings& Settings);
-    void ApplyScreenShake(FVector Location, float Intensity);
-
     UPROPERTY()
-    float GlobalIntensityMultiplier = 1.0f;
+    TArray<UParticleSystemComponent*> ActiveEffects;
 
-    UPROPERTY()
-    TArray<UNiagaraComponent*> ActiveEffects;
+    void CleanupExpiredEffects();
 };
