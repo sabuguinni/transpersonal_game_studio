5acd9329e6f450bbe0b742c7c55d030f11107208
sabuguinni <hugomartins@transpersonalinternational.com>
2026-05-29 02:47:38 +0100

    Add Performance Profiler component for real-time performance monitoring

--- Source/TranspersonalGame/Performance/Perf_PerformanceProfiler.h
+++ Source/TranspersonalGame/Performance/Perf_PerformanceProfiler.h
@@ -4,63 +4,68 @@
 #include "Engine/World.h"
 #include "GameFramework/Actor.h"
 #include "Components/ActorComponent.h"
-#include "Engine/Engine.h"
 #include "Perf_PerformanceProfiler.generated.h"
 
-// Performance metrics structure
+UENUM(BlueprintType)
+enum class EPerf_ProfilerMetric : uint8
+{
+    FrameTime,
+    RenderTime,
+    GameThreadTime,
+    RHIThreadTime,
+    GPUTime,
+    DrawCalls,
+    Triangles,
+    MemoryUsage
+};
+
 USTRUCT(BlueprintType)
 struct TRANSPERSONALGAME_API FPerf_PerformanceMetrics
 {
     GENERATED_BODY()
 
     UPROPERTY(BlueprintReadOnly, Category = "Performance")
-    float CurrentFPS;
+    float CurrentFPS = 0.0f;
 
     UPROPERTY(BlueprintReadOnly, Category = "Performance")
-    float AverageFrameTime;
+    float AverageFPS = 0.0f;
 
     UPROPERTY(BlueprintReadOnly, Category = "Performance")
-    float MemoryUsageMB;
+    float MinFPS = 0.0f;
 
     UPROPERTY(BlueprintReadOnly, Category = "Performance")
-    int32 ActorCount;
+    float MaxFPS = 0.0f;
 
     UPROPERTY(BlueprintReadOnly, Category = "Performance")
-    int32 DrawCalls;
+    float FrameTime = 0.0f;
 
     UPROPERTY(BlueprintReadOnly, Category = "Performance")
-    float GPUTime;
+    float RenderTime = 0.0f;
 
     UPROPERTY(BlueprintReadOnly, Category = "Performance")
-    float CPUTime;
+    int32 DrawCalls = 0;
+
+    UPROPERTY(BlueprintReadOnly, Category = "Performance")
+    int32 TriangleCount = 0;
+
+    UPROPERTY(BlueprintReadOnly, Category = "Performance")
+    float MemoryUsageMB = 0.0f;
 
     FPerf_PerformanceMetrics()
-        : CurrentFPS(0.0f)
-        , AverageFrameTime(0.0f)
-        , MemoryUsageMB(0.0f)
-        , ActorCount(0)
-        , DrawCalls(0)
-        , GPUTime(0.0f)
-        , CPUTime(0.0f)
-    {}
+    {
+        CurrentFPS = 60.0f;
+        AverageFPS = 60.0f;
+        MinFPS = 60.0f;
+        MaxFPS = 60.0f;
+        FrameTime = 16.67f;
+        RenderTime = 8.0f;
+        DrawCalls = 0;
+        TriangleCount = 0;
+        MemoryUsageMB = 0.0f;
+    }
 };
 
-// Performance profiling levels
-UENUM(BlueprintType)
-enum class EPerf_ProfilingLevel : uint8
-{
-    None        UMETA(DisplayName = "None"),
-    Basic       UMETA(DisplayName = "Basic"),
-    Detailed    UMETA(DisplayName = "Detailed"),
-    Advanced    UMETA(DisplayName = "Advanced"),
-    Debug       UMETA(DisplayName = "Debug")
-};
-
-/**
- * Performance Profiler Component
- * Monitors game performance in real-time and provides optimization recommendations
- */
-UCLASS(ClassGroup=(Performance), meta=(BlueprintSpawnableComponent))
+UCLASS(BlueprintType, Blueprintable)
 class TRANSPERSONALGAME_API UPerf_PerformanceProfiler : public UActorComponent
 {
     GENERATED_BODY()
@@ -73,7 +78,6 @@ protected:
     virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 public:
-    // Performance monitoring
     UFUNCTION(BlueprintCallable, Category = "Performance")
     void StartProfiling();
 
@@ -81,64 +85,43 @@ public:
     void StopProfiling();
 
     UFUNCTION(BlueprintCallable, Category = "Performance")
-    FPerf_PerformanceMetrics GetCurrentMetrics() const;
+    void ResetMetrics();
 
     UFUNCTION(BlueprintCallable, Category = "Performance")
-    void SetProfilingLevel(EPerf_ProfilingLevel NewLevel);
+    FPerf_PerformanceMetrics GetCurrentMetrics() const;
 
     UFUNCTION(BlueprintCallable, Category = "Performance")
     void LogPerformanceReport();
 
-    // Performance optimization
-    UFUNCTION(BlueprintCallable, Category = "Performance")
-    void OptimizeActorLOD(AActor* Actor, float Distance);
-
-    UFUNCTION(BlueprintCallable, Category = "Performance")
-    void CullDistantActors(float CullDistance);
-
     UFUNCTION(BlueprintCallable, Category = "Performance")
     bool IsPerformanceAcceptable() const;
 
-    // Performance targets
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Performance Targets")
-    float TargetFPS_PC;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Performance Targets")
-    float TargetFPS_Console;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Performance Targets")
-    float MaxMemoryUsageMB;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Performance Targets")
-    int32 MaxActorCount;
+    UFUNCTION(BlueprintCallable, Category = "Performance")
+    void SetTargetFPS(float NewTargetFPS);
 
 protected:
-    // Profiling state
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Performance", meta = (AllowPrivateAccess = "true"))
-    bool bIsProfiling;
-
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Performance", meta = (AllowPrivateAccess = "true"))
-    EPerf_ProfilingLevel CurrentProfilingLevel;
-
-    // Performance metrics
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Performance", meta = (AllowPrivateAccess = "true"))
+    UPROPERTY(BlueprintReadOnly, Category = "Performance")
     FPerf_PerformanceMetrics CurrentMetrics;
 
-    // Frame time history for averaging
-    TArray<float> FrameTimeHistory;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Performance")
+    float TargetFPS = 60.0f;
 
-    UPROPERTY(EditAnywhere, Category = "Performance")
-    int32 FrameHistorySize;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Performance")
+    float MinimumAcceptableFPS = 30.0f;
 
-    // Profiling timer
-    float ProfilingTimer;
-    float ProfilingInterval;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Performance")
+    bool bIsProfilingActive = false;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Performance")
+    float ProfilingInterval = 1.0f;
 
 private:
+    TArray<float> FPSSamples;
+    float LastProfilingTime = 0.0f;
+    int32 FrameCount = 0;
+    float TotalFrameTime = 0.0f;
+
     void UpdateMetrics(float DeltaTime);
-    void CollectBasicMetrics();
-    void CollectDetailedMetrics();
-    void CollectAdvancedMetrics();
-    float CalculateAverageFrameTime() const;
-    void OptimizePerformance();
+    void CollectRenderStats();
+    void CollectMemoryStats();
 };
