9bb90492a1cd92b3c0ce81463b50a87771627652
sabuguinni <hugomartins@transpersonalinternational.com>
2026-05-29 12:05:42 +0100

    Add NPC_DinosaurBehaviorTree.h - Behavior tree system for dinosaur AI with memory and state management

--- Source/TranspersonalGame/AI/NPC/NPC_DinosaurBehaviorTree.h
+++ Source/TranspersonalGame/AI/NPC/NPC_DinosaurBehaviorTree.h
@@ -2,44 +2,22 @@
 
 #include "CoreMinimal.h"
 #include "BehaviorTree/BehaviorTree.h"
-#include "BehaviorTree/BTTaskNode.h"
-#include "BehaviorTree/BTCompositeNode.h"
-#include "BehaviorTree/BTDecorator.h"
 #include "BehaviorTree/BlackboardComponent.h"
-#include "Engine/World.h"
-#include "GameFramework/Pawn.h"
-#include "GameFramework/Character.h"
-#include "Components/ActorComponent.h"
+#include "BehaviorTree/BehaviorTreeComponent.h"
+#include "Engine/Engine.h"
 #include "TranspersonalGame/TranspersonalGame.h"
 #include "NPC_DinosaurBehaviorTree.generated.h"
-
-// Forward declarations
-class APawn;
-class ACharacter;
-class UBlackboardComponent;
 
 UENUM(BlueprintType)
 enum class ENPC_DinosaurState : uint8
 {
     Idle        UMETA(DisplayName = "Idle"),
-    Patrolling  UMETA(DisplayName = "Patrolling"),
-    Hunting     UMETA(DisplayName = "Hunting"),
-    Feeding     UMETA(DisplayName = "Feeding"),
-    Fleeing     UMETA(DisplayName = "Fleeing"),
-    Sleeping    UMETA(DisplayName = "Sleeping"),
-    Drinking    UMETA(DisplayName = "Drinking"),
+    Patrol      UMETA(DisplayName = "Patrol"),
+    Hunt        UMETA(DisplayName = "Hunt"),
+    Flee        UMETA(DisplayName = "Flee"),
+    Feed        UMETA(DisplayName = "Feed"),
+    Sleep       UMETA(DisplayName = "Sleep"),
     Territorial UMETA(DisplayName = "Territorial")
-};
-
-UENUM(BlueprintType)
-enum class ENPC_DinosaurSpecies : uint8
-{
-    TRex            UMETA(DisplayName = "T-Rex"),
-    Velociraptor    UMETA(DisplayName = "Velociraptor"),
-    Triceratops     UMETA(DisplayName = "Triceratops"),
-    Brachiosaurus   UMETA(DisplayName = "Brachiosaurus"),
-    Ankylosaurus    UMETA(DisplayName = "Ankylosaurus"),
-    Parasaurolophus UMETA(DisplayName = "Parasaurolophus")
 };
 
 USTRUCT(BlueprintType)
@@ -47,65 +25,31 @@ struct FNPC_DinosaurMemory
 {
     GENERATED_BODY()
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
-    FVector LastPlayerLocation;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Memory")
+    FVector LastKnownPlayerLocation;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
-    float TimeSincePlayerSeen;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Memory")
+    float LastPlayerSightTime;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
-    float ThreatLevel;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Memory")
     TArray<FVector> PatrolPoints;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Memory")
     int32 CurrentPatrolIndex;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Memory")
+    float LastFeedTime;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Memory")
+    bool bIsInTerritory;
 
     FNPC_DinosaurMemory()
     {
-        LastPlayerLocation = FVector::ZeroVector;
-        TimeSincePlayerSeen = 0.0f;
-        ThreatLevel = 0.0f;
+        LastKnownPlayerLocation = FVector::ZeroVector;
+        LastPlayerSightTime = 0.0f;
         CurrentPatrolIndex = 0;
-    }
-};
-
-USTRUCT(BlueprintType)
-struct FNPC_DinosaurStats
-{
-    GENERATED_BODY()
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
-    float Hunger;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
-    float Energy;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
-    float Aggression;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
-    float Fear;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
-    float PatrolRadius;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
-    float DetectionRange;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
-    float AttackRange;
-
-    FNPC_DinosaurStats()
-    {
-        Hunger = 50.0f;
-        Energy = 100.0f;
-        Aggression = 30.0f;
-        Fear = 10.0f;
-        PatrolRadius = 5000.0f;
-        DetectionRange = 3000.0f;
-        AttackRange = 300.0f;
+        LastFeedTime = 0.0f;
+        bIsInTerritory = true;
     }
 };
 
@@ -117,108 +61,73 @@ class TRANSPERSONALGAME_API UNPC_DinosaurBehaviorTree : public UObject
 public:
     UNPC_DinosaurBehaviorTree();
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Behavior")
-    ENPC_DinosaurSpecies Species;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Behavior Tree")
+    class UBehaviorTree* TRexBehaviorTree;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Behavior Tree")
+    class UBehaviorTree* VelociraptorBehaviorTree;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Behavior Tree")
+    class UBehaviorTree* HerbivoreBehaviorTree;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Behavior Tree")
+    class UBlackboardData* DinosaurBlackboard;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Memory")
+    FNPC_DinosaurMemory DinosaurMemory;
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Behavior")
     ENPC_DinosaurState CurrentState;
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Behavior")
-    FNPC_DinosaurStats Stats;
+    float SightRange;
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Behavior")
-    FNPC_DinosaurMemory Memory;
+    float HearingRange;
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Behavior")
-    UBehaviorTree* BehaviorTreeAsset;
+    float TerritoryRadius;
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Behavior")
-    UBlackboardComponent* BlackboardComp;
+    float PatrolSpeed;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Behavior")
+    float ChaseSpeed;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Behavior")
+    float AttackRange;
 
     UFUNCTION(BlueprintCallable, Category = "Behavior")
-    void InitializeBehavior(APawn* OwnerPawn);
+    void InitializeBehaviorTree(class AAIController* AIController);
 
     UFUNCTION(BlueprintCallable, Category = "Behavior")
-    void UpdateBehavior(float DeltaTime);
+    void UpdateDinosaurState(ENPC_DinosaurState NewState);
 
     UFUNCTION(BlueprintCallable, Category = "Behavior")
-    void SetState(ENPC_DinosaurState NewState);
+    bool CanSeePlayer(class APawn* Player);
 
     UFUNCTION(BlueprintCallable, Category = "Behavior")
-    bool CanSeePlayer(APawn* OwnerPawn, APawn* Player);
-
-    UFUNCTION(BlueprintCallable, Category = "Behavior")
-    float GetDistanceToPlayer(APawn* OwnerPawn, APawn* Player);
+    bool IsInTerritory(FVector Location);
 
     UFUNCTION(BlueprintCallable, Category = "Behavior")
     FVector GetNextPatrolPoint();
 
-    UFUNCTION(BlueprintCallable, Category = "Behavior")
-    void UpdateMemory(APawn* Player, float DeltaTime);
+    UFUNCTION(BlueprintCallable, Category = "Memory")
+    void UpdatePlayerMemory(FVector PlayerLocation);
 
-    UFUNCTION(BlueprintCallable, Category = "Behavior")
-    void SetupSpeciesBehavior();
+    UFUNCTION(BlueprintCallable, Category = "Memory")
+    void AddPatrolPoint(FVector Point);
 
-private:
-    void SetupTRexBehavior();
-    void SetupVelociraptorBehavior();
-    void SetupTriceratopsBehavior();
-    void SetupBrachiosaurusBehavior();
+    UFUNCTION(BlueprintCallable, Category = "Memory")
+    void ClearMemory();
 
-    UPROPERTY()
-    float StateTimer;
-
+protected:
     UPROPERTY()
     FVector HomeLocation;
-};
 
-// Behavior Tree Task Nodes
-UCLASS()
-class TRANSPERSONALGAME_API UNPC_BTTask_Patrol : public UBTTaskNode
-{
-    GENERATED_BODY()
+    UPROPERTY()
+    float StateChangeTime;
 
-public:
-    UNPC_BTTask_Patrol();
-
-    virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
-};
-
-UCLASS()
-class TRANSPERSONALGAME_API UNPC_BTTask_ChasePlayer : public UBTTaskNode
-{
-    GENERATED_BODY()
-
-public:
-    UNPC_BTTask_ChasePlayer();
-
-    virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
-};
-
-UCLASS()
-class TRANSPERSONALGAME_API UNPC_BTTask_AttackPlayer : public UBTTaskNode
-{
-    GENERATED_BODY()
-
-public:
-    UNPC_BTTask_AttackPlayer();
-
-    virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
-};
-
-UCLASS()
-class TRANSPERSONALGAME_API UNPC_BTDecorator_CanSeePlayer : public UBTDecorator
-{
-    GENERATED_BODY()
-
-public:
-    UNPC_BTDecorator_CanSeePlayer();
-
-    virtual bool CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const override;
-
-    UPROPERTY(EditAnywhere, Category = "Detection")
-    float SightRange;
-
-    UPROPERTY(EditAnywhere, Category = "Detection")
-    float SightAngle;
+    UFUNCTION()
+    void OnStateChanged(ENPC_DinosaurState OldState, ENPC_DinosaurState NewState);
 };
