512e94f3e6df47743d00a611e4ae35023fb891fb
sabuguinni <hugomartins@transpersonalinternational.com>
2026-06-02 00:49:52 +0100

    Add character dialogue data structures for narrative system

--- Source/TranspersonalGame/Narrative/Narr_CharacterDialogue.h
+++ Source/TranspersonalGame/Narrative/Narr_CharacterDialogue.h
@@ -2,34 +2,8 @@
 
 #include "CoreMinimal.h"
 #include "Engine/DataAsset.h"
-#include "Sound/SoundBase.h"
+#include "SharedTypes.h"
 #include "Narr_CharacterDialogue.generated.h"
-
-UENUM(BlueprintType)
-enum class ENarr_DialogueType : uint8
-{
-    Greeting,
-    Warning,
-    Information,
-    Trade,
-    Quest,
-    Farewell,
-    Combat,
-    Emergency
-};
-
-UENUM(BlueprintType)
-enum class ENarr_CharacterRole : uint8
-{
-    TribalElder,
-    WarChief,
-    Scout,
-    CampKeeper,
-    Hunter,
-    Crafter,
-    Healer,
-    Trader
-};
 
 USTRUCT(BlueprintType)
 struct TRANSPERSONALGAME_API FNarr_DialogueLine
@@ -37,54 +11,50 @@ struct TRANSPERSONALGAME_API FNarr_DialogueLine
     GENERATED_BODY()
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")
+    FString SpeakerName;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")
     FText DialogueText;
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")
-    TSoftObjectPtr<USoundBase> VoiceClip;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")
-    ENarr_DialogueType DialogueType;
+    FString AudioPath;
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")
     float Duration;
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")
-    TArray<FText> PlayerResponses;
+    TArray<FString> ResponseOptions;
 
     FNarr_DialogueLine()
     {
-        DialogueType = ENarr_DialogueType::Information;
+        SpeakerName = TEXT("Unknown");
+        DialogueText = FText::GetEmpty();
+        AudioPath = TEXT("");
         Duration = 3.0f;
     }
 };
 
 USTRUCT(BlueprintType)
-struct TRANSPERSONALGAME_API FNarr_CharacterProfile
+struct TRANSPERSONALGAME_API FNarr_DialogueSequence
 {
     GENERATED_BODY()
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Character")
-    FText CharacterName;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")
+    FString SequenceID;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Character")
-    ENarr_CharacterRole Role;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Character")
-    FText BackgroundStory;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Character")
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")
     TArray<FNarr_DialogueLine> DialogueLines;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Character")
-    TArray<FString> KnownTopics;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")
+    TArray<FString> Prerequisites;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Character")
-    float TrustLevel;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")
+    bool bIsRepeatable;
 
-    FNarr_CharacterProfile()
+    FNarr_DialogueSequence()
     {
-        Role = ENarr_CharacterRole::CampKeeper;
-        TrustLevel = 0.5f;
+        SequenceID = TEXT("default");
+        bIsRepeatable = true;
     }
 };
 
@@ -97,26 +67,29 @@ public:
     UNarr_CharacterDialogue();
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Character")
-    FNarr_CharacterProfile CharacterProfile;
+    FString CharacterName;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Character")
+    ENarr_NPCType NPCType;
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")
-    TMap<ENarr_DialogueType, TArray<FNarr_DialogueLine>> DialoguesByType;
+    TArray<FNarr_DialogueSequence> DialogueSequences;
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")
-    TArray<FNarr_DialogueLine> ContextualDialogue;
+    FNarr_DialogueSequence DefaultGreeting;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")
+    FNarr_DialogueSequence EmergencyWarning;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Audio")
+    TArray<FString> VoiceAssetPaths;
 
     UFUNCTION(BlueprintCallable, Category = "Dialogue")
-    FNarr_DialogueLine GetRandomDialogue(ENarr_DialogueType Type);
+    FNarr_DialogueSequence GetDialogueByID(const FString& SequenceID);
 
     UFUNCTION(BlueprintCallable, Category = "Dialogue")
-    TArray<FNarr_DialogueLine> GetDialogueByType(ENarr_DialogueType Type);
+    TArray<FString> GetAvailableSequences();
 
     UFUNCTION(BlueprintCallable, Category = "Dialogue")
-    bool HasDialogueForType(ENarr_DialogueType Type);
-
-    UFUNCTION(BlueprintCallable, Category = "Character")
-    void ModifyTrustLevel(float Delta);
-
-    UFUNCTION(BlueprintCallable, Category = "Character")
-    bool CanDiscussTopics(const TArray<FString>& Topics);
+    bool HasPrerequisites(const FString& SequenceID, const TArray<FString>& PlayerProgress);
 };
