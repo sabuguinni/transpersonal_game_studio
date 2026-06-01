0139c4791e93a4a4e0c85ab4c7a3797f8ae5322b
sabuguinni <hugomartins@transpersonalinternational.com>
2026-06-01 16:19:30 +0100

    Add Narr_StoryManager header - story event and dialogue system

--- Source/TranspersonalGame/Narrative/Narr_StoryManager.h
+++ Source/TranspersonalGame/Narrative/Narr_StoryManager.h
@@ -5,9 +5,6 @@
 #include "SharedTypes.h"
 #include "Narr_StoryManager.generated.h"
 
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnStoryEventTriggered, FString, EventID, FString, EventDescription);
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnStoryMilestoneReached, EStoryMilestone, Milestone, FString, Description);
-
 USTRUCT(BlueprintType)
 struct TRANSPERSONALGAME_API FNarr_StoryEvent
 {
@@ -17,71 +14,54 @@ struct TRANSPERSONALGAME_API FNarr_StoryEvent
     FString EventID;
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Story")
-    FString EventName;
+    FString EventTitle;
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Story")
     FString EventDescription;
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Story")
-    EStoryMilestone RequiredMilestone;
+    bool bIsCompleted;
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Story")
-    TArray<FString> PrerequisiteEvents;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Story")
-    bool bIsTriggered;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Story")
-    bool bIsRepeatable;
+    int32 Priority;
 
     FNarr_StoryEvent()
     {
         EventID = TEXT("");
-        EventName = TEXT("");
+        EventTitle = TEXT("");
         EventDescription = TEXT("");
-        RequiredMilestone = EStoryMilestone::None;
-        bIsTriggered = false;
-        bIsRepeatable = false;
+        bIsCompleted = false;
+        Priority = 0;
     }
 };
 
 USTRUCT(BlueprintType)
-struct TRANSPERSONALGAME_API FNarr_StoryProgress
+struct TRANSPERSONALGAME_API FNarr_DialogueLine
 {
     GENERATED_BODY()
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Progress")
-    EStoryMilestone CurrentMilestone;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")
+    FString SpeakerName;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Progress")
-    TArray<FString> CompletedEvents;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")
+    FString DialogueText;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Progress")
-    TArray<FString> ActiveEvents;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")
+    FString AudioPath;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Progress")
-    int32 DinosaurEncounters;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")
+    float Duration;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Progress")
-    int32 ResourcesGathered;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Progress")
-    int32 ToolsCrafted;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Progress")
-    float SurvivalTime;
-
-    FNarr_StoryProgress()
+    FNarr_DialogueLine()
     {
-        CurrentMilestone = EStoryMilestone::None;
-        DinosaurEncounters = 0;
-        ResourcesGathered = 0;
-        ToolsCrafted = 0;
-        SurvivalTime = 0.0f;
+        SpeakerName = TEXT("");
+        DialogueText = TEXT("");
+        AudioPath = TEXT("");
+        Duration = 3.0f;
     }
 };
 
-UCLASS(BlueprintType, Blueprintable)
+UCLASS()
 class TRANSPERSONALGAME_API UNarr_StoryManager : public UGameInstanceSubsystem
 {
     GENERATED_BODY()
@@ -89,74 +69,37 @@ class TRANSPERSONALGAME_API UNarr_StoryManager : public UGameInstanceSubsystem
 public:
     UNarr_StoryManager();
 
-    // Subsystem interface
     virtual void Initialize(FSubsystemCollectionBase& Collection) override;
-    virtual void Deinitialize() override;
 
-    // Story progression
     UFUNCTION(BlueprintCallable, Category = "Story")
     void TriggerStoryEvent(const FString& EventID);
 
     UFUNCTION(BlueprintCallable, Category = "Story")
-    void AdvanceStoryMilestone(EStoryMilestone NewMilestone);
+    bool IsStoryEventCompleted(const FString& EventID);
 
     UFUNCTION(BlueprintCallable, Category = "Story")
-    bool CanTriggerEvent(const FString& EventID) const;
+    void PlayDialogue(const FString& DialogueID);
 
     UFUNCTION(BlueprintCallable, Category = "Story")
-    EStoryMilestone GetCurrentMilestone() const;
+    void RegisterStoryEvent(const FNarr_StoryEvent& NewEvent);
 
     UFUNCTION(BlueprintCallable, Category = "Story")
-    FNarr_StoryProgress GetStoryProgress() const;
-
-    // Event registration
-    UFUNCTION(BlueprintCallable, Category = "Story")
-    void RegisterStoryEvent(const FNarr_StoryEvent& StoryEvent);
-
-    UFUNCTION(BlueprintCallable, Category = "Story")
-    void RegisterDefaultStoryEvents();
-
-    // Progress tracking
-    UFUNCTION(BlueprintCallable, Category = "Progress")
-    void IncrementDinosaurEncounters();
-
-    UFUNCTION(BlueprintCallable, Category = "Progress")
-    void IncrementResourcesGathered();
-
-    UFUNCTION(BlueprintCallable, Category = "Progress")
-    void IncrementToolsCrafted();
-
-    UFUNCTION(BlueprintCallable, Category = "Progress")
-    void UpdateSurvivalTime(float DeltaTime);
-
-    // Events
-    UPROPERTY(BlueprintAssignable, Category = "Events")
-    FOnStoryEventTriggered OnStoryEventTriggered;
-
-    UPROPERTY(BlueprintAssignable, Category = "Events")
-    FOnStoryMilestoneReached OnStoryMilestoneReached;
-
-    // Contextual narration
-    UFUNCTION(BlueprintCallable, Category = "Narration")
-    FString GetContextualNarration(const FString& Context) const;
-
-    UFUNCTION(BlueprintCallable, Category = "Narration")
-    void TriggerEnvironmentalNarration(const FString& BiomeName, const FString& WeatherCondition);
+    TArray<FNarr_StoryEvent> GetActiveStoryEvents();
 
 protected:
-    UPROPERTY(BlueprintReadOnly, Category = "Data")
-    TMap<FString, FNarr_StoryEvent> StoryEvents;
+    UPROPERTY(BlueprintReadOnly, Category = "Story")
+    TArray<FNarr_StoryEvent> StoryEvents;
 
-    UPROPERTY(BlueprintReadOnly, Category = "Progress")
-    FNarr_StoryProgress CurrentProgress;
+    UPROPERTY(BlueprintReadOnly, Category = "Story")
+    TMap<FString, FNarr_DialogueLine> DialogueDatabase;
 
-    UPROPERTY(BlueprintReadOnly, Category = "Data")
-    TMap<FString, FString> ContextualNarrations;
+    UPROPERTY(BlueprintReadOnly, Category = "Story")
+    FString CurrentDialogueID;
+
+    UPROPERTY(BlueprintReadOnly, Category = "Story")
+    bool bIsDialoguePlaying;
 
 private:
-    void CheckMilestoneProgression();
-    void RegisterContextualNarrations();
-    bool ArePrerequisitesMet(const FNarr_StoryEvent& Event) const;
+    void InitializeStoryEvents();
+    void LoadDialogueDatabase();
 };
-
-#include "Narr_StoryManager.generated.h"
