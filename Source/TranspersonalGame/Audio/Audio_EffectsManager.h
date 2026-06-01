6973ac2fa04d2494b413400417c8ebfbf2911f34
sabuguinni <hugomartins@transpersonalinternational.com>
2026-06-01 05:11:53 +0100

    Audio Agent #16 - Create Audio Effects Manager system for prehistoric survival game

--- Source/TranspersonalGame/Audio/Audio_EffectsManager.h
+++ Source/TranspersonalGame/Audio/Audio_EffectsManager.h
@@ -4,56 +4,50 @@
 #include "GameFramework/Actor.h"
 #include "Components/AudioComponent.h"
 #include "Sound/SoundCue.h"
-#include "Engine/World.h"
-#include "GameFramework/PlayerController.h"
-#include "Camera/CameraShakeBase.h"
+#include "Engine/TriggerVolume.h"
 #include "Audio_EffectsManager.generated.h"
 
-USTRUCT(BlueprintType)
-struct TRANSPERSONALGAME_API FAudio_ScreenShakeSettings
+UENUM(BlueprintType)
+enum class EAudio_EffectType : uint8
 {
-    GENERATED_BODY()
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Screen Shake")
-    float Intensity = 1.0f;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Screen Shake")
-    float Duration = 0.5f;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Screen Shake")
-    float Frequency = 10.0f;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Screen Shake")
-    float Range = 2000.0f;
-
-    FAudio_ScreenShakeSettings()
-    {
-        Intensity = 1.0f;
-        Duration = 0.5f;
-        Frequency = 10.0f;
-        Range = 2000.0f;
-    }
+    Footstep,
+    Roar,
+    Ambient,
+    Impact,
+    Environmental,
+    UI
 };
 
 USTRUCT(BlueprintType)
-struct TRANSPERSONALGAME_API FAudio_DamageFlashSettings
+struct FAudio_EffectData
 {
     GENERATED_BODY()
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Damage Flash")
-    FLinearColor FlashColor = FLinearColor::Red;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Audio Effect")
+    EAudio_EffectType EffectType;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Damage Flash")
-    float FlashDuration = 0.3f;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Audio Effect")
+    TSoftObjectPtr<USoundCue> SoundCue;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Damage Flash")
-    float FlashIntensity = 0.7f;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Audio Effect")
+    float Volume = 1.0f;
 
-    FAudio_DamageFlashSettings()
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Audio Effect")
+    float Pitch = 1.0f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Audio Effect")
+    float MaxDistance = 5000.0f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Audio Effect")
+    bool bIs3D = true;
+
+    FAudio_EffectData()
     {
-        FlashColor = FLinearColor::Red;
-        FlashDuration = 0.3f;
-        FlashIntensity = 0.7f;
+        EffectType = EAudio_EffectType::Ambient;
+        Volume = 1.0f;
+        Pitch = 1.0f;
+        MaxDistance = 5000.0f;
+        bIs3D = true;
     }
 };
 
@@ -68,49 +62,58 @@ public:
 protected:
     virtual void BeginPlay() override;
 
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Audio")
-    class UAudioComponent* AudioComponent;
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Audio Components")
+    class USceneComponent* RootSceneComponent;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Screen Shake")
-    FAudio_ScreenShakeSettings TRexFootstepShake;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Audio Effects")
+    TMap<EAudio_EffectType, FAudio_EffectData> EffectLibrary;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Screen Shake")
-    FAudio_ScreenShakeSettings RaptorFootstepShake;
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Audio Components")
+    TArray<UAudioComponent*> ActiveAudioComponents;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Damage Flash")
-    FAudio_DamageFlashSettings DamageFlash;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Audio Settings")
+    float MasterVolume = 1.0f;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Audio")
-    USoundCue* TRexFootstepSound;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Audio Settings")
+    float EffectsVolume = 0.8f;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Audio")
-    USoundCue* RaptorFootstepSound;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Audio")
-    USoundCue* DamageSound;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Audio Settings")
+    float MaxSimultaneousEffects = 32;
 
 public:
     virtual void Tick(float DeltaTime) override;
 
-    UFUNCTION(BlueprintCallable, Category = "Effects")
-    void TriggerTRexFootstep(FVector Location);
+    UFUNCTION(BlueprintCallable, Category = "Audio Effects")
+    void PlayEffect(EAudio_EffectType EffectType, FVector Location, float VolumeMultiplier = 1.0f);
 
-    UFUNCTION(BlueprintCallable, Category = "Effects")
-    void TriggerRaptorFootstep(FVector Location);
+    UFUNCTION(BlueprintCallable, Category = "Audio Effects")
+    void PlayEffectAtActor(EAudio_EffectType EffectType, AActor* TargetActor, float VolumeMultiplier = 1.0f);
 
-    UFUNCTION(BlueprintCallable, Category = "Effects")
-    void TriggerDamageFlash(float DamageAmount);
+    UFUNCTION(BlueprintCallable, Category = "Audio Effects")
+    void StopAllEffects();
 
-    UFUNCTION(BlueprintCallable, Category = "Effects")
-    void TriggerScreenShake(FAudio_ScreenShakeSettings ShakeSettings, FVector EpicenterLocation);
+    UFUNCTION(BlueprintCallable, Category = "Audio Effects")
+    void StopEffectType(EAudio_EffectType EffectType);
 
-    UFUNCTION(BlueprintCallable, Category = "Effects")
-    void PlaySoundAtLocation(USoundCue* SoundCue, FVector Location, float VolumeMultiplier = 1.0f);
+    UFUNCTION(BlueprintCallable, Category = "Audio Settings")
+    void SetMasterVolume(float NewVolume);
 
-private:
-    void ApplyScreenShake(float Intensity, float Duration, float Frequency, FVector EpicenterLocation, float Range);
-    void ApplyDamageFlash(FAudio_DamageFlashSettings FlashSettings);
-    
-    float GetDistanceToPlayer(FVector Location);
-    APlayerController* GetPlayerController();
+    UFUNCTION(BlueprintCallable, Category = "Audio Settings")
+    void SetEffectsVolume(float NewVolume);
+
+    UFUNCTION(BlueprintCallable, Category = "Audio Effects")
+    bool IsEffectPlaying(EAudio_EffectType EffectType) const;
+
+    UFUNCTION(BlueprintCallable, Category = "Audio Effects")
+    int32 GetActiveEffectCount() const;
+
+protected:
+    UFUNCTION()
+    void CleanupFinishedComponents();
+
+    UFUNCTION()
+    void InitializeEffectLibrary();
+
+    UFUNCTION()
+    UAudioComponent* CreateAudioComponent(const FAudio_EffectData& EffectData, FVector Location);
 };
