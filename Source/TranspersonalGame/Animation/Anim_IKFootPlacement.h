5d28d623e26215987a7bae8abb3bd88d95896118
sabuguinni <hugomartins@transpersonalinternational.com>
2026-06-02 11:09:37 +0100

    Animation Agent #10: Add IK foot placement component for terrain adaptation

--- Source/TranspersonalGame/Animation/Anim_IKFootPlacement.h
+++ Source/TranspersonalGame/Animation/Anim_IKFootPlacement.h
@@ -1,1 +1,80 @@
-undefined
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Engine/Engine.h"
+#include "Engine/World.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "Animation/AnimInstance.h"
+#include "Anim_IKFootPlacement.generated.h"
+
+USTRUCT(BlueprintType)
+struct TRANSPERSONALGAME_API FAnim_FootIKData
+{
+    GENERATED_BODY()
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "IK")
+    FVector FootOffset;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "IK")
+    FRotator FootRotation;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "IK")
+    float IKAlpha;
+
+    FAnim_FootIKData()
+    {
+        FootOffset = FVector::ZeroVector;
+        FootRotation = FRotator::ZeroRotator;
+        IKAlpha = 0.0f;
+    }
+};
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class TRANSPERSONALGAME_API UAnim_IKFootPlacement : public UActorComponent
+{
+    GENERATED_BODY()
+
+public:
+    UAnim_IKFootPlacement();
+
+protected:
+    virtual void BeginPlay() override;
+
+public:
+    virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "IK Settings")
+    float TraceDistance;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "IK Settings")
+    float IKInterpSpeed;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "IK Settings")
+    float MaxFootOffset;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "IK Settings")
+    FName LeftFootBoneName;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "IK Settings")
+    FName RightFootBoneName;
+
+    UPROPERTY(BlueprintReadOnly, Category = "IK Data")
+    FAnim_FootIKData LeftFootIK;
+
+    UPROPERTY(BlueprintReadOnly, Category = "IK Data")
+    FAnim_FootIKData RightFootIK;
+
+    UFUNCTION(BlueprintCallable, Category = "IK")
+    void UpdateFootIK();
+
+    UFUNCTION(BlueprintCallable, Category = "IK")
+    FAnim_FootIKData CalculateFootIK(const FName& FootBoneName, const FVector& FootLocation);
+
+private:
+    UPROPERTY()
+    USkeletalMeshComponent* OwnerMesh;
+
+    FVector TraceForGround(const FVector& StartLocation) const;
+    FRotator CalculateFootRotation(const FVector& ImpactNormal) const;
+};
