b15be52d62e62b436b4449036b0548c87094c72d
sabuguinni <hugomartins@transpersonalinternational.com>
2026-06-11 08:20:49 +0100

    Add Anim_CharacterAnimInstance header - comprehensive animation state management for prehistoric character

--- Source/TranspersonalGame/Animation/Anim_CharacterAnimInstance.h
+++ Source/TranspersonalGame/Animation/Anim_CharacterAnimInstance.h
@@ -1,1 +1,105 @@
-undefined
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "../SharedTypes.h"
+#include "Anim_CharacterAnimInstance.generated.h"
+
+UCLASS(BlueprintType, Blueprintable)
+class TRANSPERSONALGAME_API UAnim_CharacterAnimInstance : public UAnimInstance
+{
+    GENERATED_BODY()
+
+public:
+    UAnim_CharacterAnimInstance();
+
+    virtual void NativeInitializeAnimation() override;
+    virtual void NativeUpdateAnimation(float DeltaTimeX) override;
+
+protected:
+    // Movement State Variables
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
+    float Speed;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
+    float Direction;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
+    bool bIsInAir;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
+    bool bIsAccelerating;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
+    bool bIsCrouching;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
+    bool bIsRunning;
+
+    // Survival State Variables
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Survival", meta = (AllowPrivateAccess = "true"))
+    float HealthPercent;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Survival", meta = (AllowPrivateAccess = "true"))
+    float StaminaPercent;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Survival", meta = (AllowPrivateAccess = "true"))
+    float FearLevel;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Survival", meta = (AllowPrivateAccess = "true"))
+    ESurvivalState SurvivalState;
+
+    // Animation Blend Variables
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Animation", meta = (AllowPrivateAccess = "true"))
+    float IdleToWalkBlend;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Animation", meta = (AllowPrivateAccess = "true"))
+    float WalkToRunBlend;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Animation", meta = (AllowPrivateAccess = "true"))
+    float FearIntensity;
+
+    // IK Variables
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "IK", meta = (AllowPrivateAccess = "true"))
+    float LeftFootIKOffset;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "IK", meta = (AllowPrivateAccess = "true"))
+    float RightFootIKOffset;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "IK", meta = (AllowPrivateAccess = "true"))
+    FRotator LeftFootIKRotation;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "IK", meta = (AllowPrivateAccess = "true"))
+    FRotator RightFootIKRotation;
+
+    // Combat Variables
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Combat", meta = (AllowPrivateAccess = "true"))
+    bool bIsInCombat;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Combat", meta = (AllowPrivateAccess = "true"))
+    EWeaponType CurrentWeaponType;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Combat", meta = (AllowPrivateAccess = "true"))
+    bool bIsAttacking;
+
+private:
+    // Cached references
+    UPROPERTY()
+    class ACharacter* Character;
+
+    UPROPERTY()
+    class UCharacterMovementComponent* MovementComponent;
+
+    // Helper functions
+    void UpdateMovementVariables();
+    void UpdateSurvivalVariables();
+    void UpdateIKVariables(float DeltaTime);
+    void UpdateCombatVariables();
+
+    // IK Helper functions
+    float GetFootIKOffset(FName SocketName, float TraceDistance = 50.0f);
+    FRotator GetFootIKRotation(FName SocketName);
+    bool LineTraceFromSocket(FName SocketName, float TraceDistance, FHitResult& OutHit);
+};
