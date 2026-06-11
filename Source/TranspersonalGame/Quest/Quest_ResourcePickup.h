20eebea042aec567d1aa08b97842087cb077ce47
sabuguinni <hugomartins@transpersonalinternational.com>
2026-06-11 21:14:34 +0100

    Add Quest_ResourcePickup header - pickupable resources for crafting system

--- Source/TranspersonalGame/Quest/Quest_ResourcePickup.h
+++ Source/TranspersonalGame/Quest/Quest_ResourcePickup.h
@@ -1,14 +1,26 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Engine/StaticMeshActor.h"
+#include "GameFramework/Actor.h"
 #include "Components/StaticMeshComponent.h"
 #include "Components/SphereComponent.h"
-#include "SharedTypes.h"
+#include "Engine/Engine.h"
+#include "../SharedTypes.h"
 #include "Quest_ResourcePickup.generated.h"
 
+UENUM(BlueprintType)
+enum class EQuest_ResourceType : uint8
+{
+    Rock        UMETA(DisplayName = "Rock"),
+    Stick       UMETA(DisplayName = "Stick"),
+    Leaf        UMETA(DisplayName = "Leaf"),
+    Fiber       UMETA(DisplayName = "Fiber"),
+    Stone       UMETA(DisplayName = "Stone"),
+    Wood        UMETA(DisplayName = "Wood")
+};
+
 UCLASS(BlueprintType, Blueprintable)
-class TRANSPERSONALGAME_API AQuest_ResourcePickup : public AStaticMeshActor
+class TRANSPERSONALGAME_API AQuest_ResourcePickup : public AActor
 {
     GENERATED_BODY()
 
@@ -18,37 +30,90 @@ public:
 protected:
     virtual void BeginPlay() override;
 
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Pickup", meta = (AllowPrivateAccess = "true"))
-    class USphereComponent* PickupCollision;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Resource")
-    EResourceType ResourceType;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Resource")
-    int32 ResourceAmount;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Resource")
-    float RespawnTime;
-
-    UPROPERTY(BlueprintReadOnly, Category = "Resource")
-    bool bIsPickedUp;
-
-    UPROPERTY(BlueprintReadOnly, Category = "Resource")
-    float RespawnTimer;
-
 public:
-    UFUNCTION(BlueprintCallable, Category = "Pickup")
-    void PickupResource(class ATranspersonalCharacter* Character);
-
-    UFUNCTION(BlueprintCallable, Category = "Pickup")
-    void RespawnResource();
-
-    UFUNCTION()
-    void OnPickupOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
-
     virtual void Tick(float DeltaTime) override;
 
-    FORCEINLINE EResourceType GetResourceType() const { return ResourceType; }
-    FORCEINLINE int32 GetResourceAmount() const { return ResourceAmount; }
-    FORCEINLINE bool IsPickedUp() const { return bIsPickedUp; }
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+    UStaticMeshComponent* MeshComponent;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+    USphereComponent* CollisionComponent;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Resource")
+    EQuest_ResourceType ResourceType;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Resource")
+    FString ResourceName;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Resource")
+    int32 ResourceQuantity;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Resource")
+    float PickupRange;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Resource")
+    bool bCanBePickedUp;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Visual")
+    bool bFloatAnimation;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Visual")
+    float FloatAmplitude;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Visual")
+    float FloatSpeed;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Visual")
+    bool bRotateAnimation;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Visual")
+    float RotationSpeed;
+
+private:
+    UPROPERTY()
+    float FloatTimer;
+
+    UPROPERTY()
+    FVector InitialLocation;
+
+    UPROPERTY()
+    FRotator InitialRotation;
+
+public:
+    UFUNCTION(BlueprintCallable, Category = "Resource")
+    void InitializeResource(EQuest_ResourceType Type, int32 Quantity = 1);
+
+    UFUNCTION(BlueprintCallable, Category = "Resource")
+    FString GetResourceDisplayName() const;
+
+    UFUNCTION(BlueprintCallable, Category = "Resource")
+    void SetupVisualEffects();
+
+    UFUNCTION(BlueprintCallable, Category = "Pickup")
+    bool CanPlayerPickup(AActor* Player) const;
+
+    UFUNCTION(BlueprintCallable, Category = "Pickup")
+    void PickupResource(AActor* Player);
+
+    UFUNCTION()
+    void OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, 
+                       UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex, 
+                       bool bFromSweep, const FHitResult& SweepResult);
+
+    UFUNCTION()
+    void OnEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, 
+                     UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex);
+
+protected:
+    UFUNCTION(BlueprintCallable, Category = "Animation")
+    void UpdateFloatAnimation(float DeltaTime);
+
+    UFUNCTION(BlueprintCallable, Category = "Animation")
+    void UpdateRotationAnimation(float DeltaTime);
+
+    UFUNCTION(BlueprintCallable, Category = "Setup")
+    void SetupMeshForResourceType();
+
+    UFUNCTION(BlueprintCallable, Category = "Debug")
+    void DebugDrawPickupRange();
 };
