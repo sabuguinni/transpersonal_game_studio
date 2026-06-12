51a2321b0d62d1ec991421b7ae582f7bef5e0ca0
sabuguinni <hugomartins@transpersonalinternational.com>
2026-06-12 13:46:54 +0100

    Add MetaSound controller for prehistoric audio system with dynamic parameter control

--- Source/TranspersonalGame/Audio/Audio_MetaSoundController.h
+++ Source/TranspersonalGame/Audio/Audio_MetaSoundController.h
@@ -2,54 +2,36 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "Engine/Engine.h"
+#include "Sound/SoundBase.h"
 #include "MetasoundSource.h"
-#include "MetasoundParameterTransmitter.h"
-#include "Components/AudioComponent.h"
 #include "Audio_MetaSoundController.generated.h"
 
-UENUM(BlueprintType)
-enum class EAudio_MetaSoundTrigger : uint8
-{
-    None = 0,
-    DinosaurApproach,
-    DangerLevel,
-    HeartbeatIntensity,
-    EnvironmentTransition,
-    CombatStart,
-    CombatEnd,
-    Crafting,
-    Discovery,
-    WeatherChange,
-    DayNightTransition
-};
-
 USTRUCT(BlueprintType)
-struct TRANSPERSONALGAME_API FAudio_MetaSoundParameter
+struct TRANSPERSONALGAME_API FAudio_MetaSoundConfig
 {
     GENERATED_BODY()
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
-    FName ParameterName;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MetaSound Config")
+    TSoftObjectPtr<UMetaSoundSource> MetaSoundAsset;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
-    float FloatValue = 0.0f;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MetaSound Config")
+    FString ParameterName;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
-    int32 IntValue = 0;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MetaSound Config")
+    float DefaultValue;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
-    bool BoolValue = false;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MetaSound Config")
+    float MinValue;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
-    EAudio_MetaSoundTrigger TriggerType = EAudio_MetaSoundTrigger::None;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MetaSound Config")
+    float MaxValue;
 
-    FAudio_MetaSoundParameter()
+    FAudio_MetaSoundConfig()
+        : DefaultValue(1.0f)
+        , MinValue(0.0f)
+        , MaxValue(1.0f)
     {
-        ParameterName = NAME_None;
-        FloatValue = 0.0f;
-        IntValue = 0;
-        BoolValue = false;
-        TriggerType = EAudio_MetaSoundTrigger::None;
     }
 };
 
@@ -63,69 +45,60 @@ public:
 
 protected:
     virtual void BeginPlay() override;
-    virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 public:
-    UFUNCTION(BlueprintCallable, Category = "MetaSound")
-    void PlayMetaSound(UMetaSoundSource* MetaSound, FVector Location = FVector::ZeroVector);
+    virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-    UFUNCTION(BlueprintCallable, Category = "MetaSound")
-    void StopMetaSound();
+    // MetaSound configuration
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MetaSound")
+    TArray<FAudio_MetaSoundConfig> MetaSoundConfigs;
 
+    // Dynamic parameter control
     UFUNCTION(BlueprintCallable, Category = "MetaSound")
-    void SetFloatParameter(FName ParameterName, float Value);
-
-    UFUNCTION(BlueprintCallable, Category = "MetaSound")
-    void SetIntParameter(FName ParameterName, int32 Value);
+    void SetMetaSoundParameter(const FString& ParameterName, float Value);
 
     UFUNCTION(BlueprintCallable, Category = "MetaSound")
-    void SetBoolParameter(FName ParameterName, bool Value);
+    void PlayMetaSound(const FString& ConfigName);
 
     UFUNCTION(BlueprintCallable, Category = "MetaSound")
-    void TriggerEvent(EAudio_MetaSoundTrigger TriggerType, float Intensity = 1.0f);
+    void StopMetaSound(const FString& ConfigName);
 
-    UFUNCTION(BlueprintCallable, Category = "MetaSound")
-    void UpdateDangerLevel(float DangerLevel);
+    // Prehistoric-specific audio controls
+    UFUNCTION(BlueprintCallable, Category = "Prehistoric Audio")
+    void PlayDinosaurFootsteps(float Intensity, float Speed);
 
-    UFUNCTION(BlueprintCallable, Category = "MetaSound")
-    void UpdateHeartbeatIntensity(float Intensity);
+    UFUNCTION(BlueprintCallable, Category = "Prehistoric Audio")
+    void PlayDinosaurRoar(float Aggression, float Distance);
 
-    UFUNCTION(BlueprintCallable, Category = "MetaSound")
-    void UpdateEnvironmentAmbience(const FString& BiomeType, float Intensity);
+    UFUNCTION(BlueprintCallable, Category = "Prehistoric Audio")
+    void UpdateAmbientForest(float TimeOfDay, float WeatherIntensity);
 
-    UFUNCTION(BlueprintCallable, Category = "MetaSound")
-    void RegisterMetaSoundAsset(EAudio_MetaSoundTrigger TriggerType, UMetaSoundSource* MetaSound);
+    UFUNCTION(BlueprintCallable, Category = "Prehistoric Audio")
+    void PlayCraftingSound(const FString& MaterialType);
 
-    UFUNCTION(BlueprintCallable, Category = "MetaSound")
-    bool IsMetaSoundPlaying() const;
+private:
+    // Internal MetaSound management
+    UPROPERTY()
+    TMap<FString, class UAudioComponent*> ActiveMetaSounds;
 
-    UFUNCTION(BlueprintCallable, Category = "MetaSound")
-    void SetMasterVolume(float Volume);
+    // Parameter interpolation
+    UPROPERTY()
+    TMap<FString, float> TargetParameters;
 
-protected:
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Audio")
-    UAudioComponent* AudioComponent;
+    UPROPERTY()
+    TMap<FString, float> CurrentParameters;
 
-    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "MetaSound")
-    TMap<EAudio_MetaSoundTrigger, UMetaSoundSource*> MetaSoundAssets;
+    UPROPERTY(EditAnywhere, Category = "MetaSound")
+    float ParameterInterpolationSpeed;
 
-    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "MetaSound")
-    UMetaSoundSource* CurrentMetaSound;
+    // Prehistoric audio state
+    UPROPERTY()
+    float CurrentFootstepIntensity;
 
-    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "MetaSound")
-    TArray<FAudio_MetaSoundParameter> ActiveParameters;
+    UPROPERTY()
+    float CurrentAmbientLevel;
 
-    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "MetaSound")
-    float MasterVolume = 1.0f;
-
-    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "MetaSound")
-    float CurrentDangerLevel = 0.0f;
-
-    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "MetaSound")
-    float CurrentHeartbeatIntensity = 0.0f;
-
-    void InitializeAudioComponent();
-    void ApplyParametersToMetaSound();
-    void UpdateParameterValue(FName ParameterName, float Value);
-    UMetaSoundSource* GetMetaSoundForTrigger(EAudio_MetaSoundTrigger TriggerType) const;
+    void InitializeMetaSounds();
+    void UpdateParameterInterpolation(float DeltaTime);
+    UAudioComponent* GetOrCreateAudioComponent(const FString& ConfigName);
 };
