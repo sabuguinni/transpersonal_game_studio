e70464fbf2e23fd509de199c1a4550511997db37
sabuguinni <hugomartins@transpersonalinternational.com>
2026-06-12 19:02:46 +0100

    Add character visual customization system with presets

--- Source/TranspersonalGame/Character/Char_VisualCustomization.h
+++ Source/TranspersonalGame/Character/Char_VisualCustomization.h
@@ -4,36 +4,33 @@
 #include "Components/ActorComponent.h"
 #include "Engine/SkeletalMesh.h"
 #include "Materials/MaterialInterface.h"
-#include "SharedTypes.h"
 #include "Char_VisualCustomization.generated.h"
 
 UENUM(BlueprintType)
 enum class EChar_SkinTone : uint8
 {
-    Fair        UMETA(DisplayName = "Fair"),
-    Medium      UMETA(DisplayName = "Medium"), 
-    Olive       UMETA(DisplayName = "Olive"),
+    Light       UMETA(DisplayName = "Light"),
+    Medium      UMETA(DisplayName = "Medium"),
     Dark        UMETA(DisplayName = "Dark"),
-    Weathered   UMETA(DisplayName = "Weathered")
+    Tanned      UMETA(DisplayName = "Tanned")
 };
 
 UENUM(BlueprintType)
-enum class EChar_BodyBuild : uint8
+enum class EChar_HairStyle : uint8
 {
-    Lean        UMETA(DisplayName = "Lean"),
-    Athletic    UMETA(DisplayName = "Athletic"),
-    Muscular    UMETA(DisplayName = "Muscular"),
-    Stocky      UMETA(DisplayName = "Stocky")
+    Long        UMETA(DisplayName = "Long"),
+    Braided     UMETA(DisplayName = "Braided"),
+    Short       UMETA(DisplayName = "Short"),
+    Shaved      UMETA(DisplayName = "Shaved")
 };
 
 UENUM(BlueprintType)
-enum class EChar_TribalMarkings : uint8
+enum class EChar_ClothingStyle : uint8
 {
-    None        UMETA(DisplayName = "None"),
-    Hunter      UMETA(DisplayName = "Hunter"),
-    Warrior     UMETA(DisplayName = "Warrior"),
-    Shaman      UMETA(DisplayName = "Shaman"),
-    Scout       UMETA(DisplayName = "Scout")
+    Leather     UMETA(DisplayName = "Leather"),
+    Fur         UMETA(DisplayName = "Fur"),
+    Woven       UMETA(DisplayName = "Woven"),
+    Minimal     UMETA(DisplayName = "Minimal")
 };
 
 USTRUCT(BlueprintType)
@@ -48,19 +45,13 @@ struct TRANSPERSONALGAME_API FChar_VisualPreset
     EChar_SkinTone SkinTone;
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Visual")
-    EChar_BodyBuild BodyBuild;
+    EChar_HairStyle HairStyle;
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Visual")
-    EChar_TribalMarkings TribalMarkings;
+    EChar_ClothingStyle ClothingStyle;
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Visual")
-    float ScarIntensity;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Visual")
-    float MuscleDefinition;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Visual")
-    bool bHasFacialHair;
+    FLinearColor FacePaintColor;
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Visual")
     TSoftObjectPtr<USkeletalMesh> CharacterMesh;
@@ -72,11 +63,9 @@ struct TRANSPERSONALGAME_API FChar_VisualPreset
     {
         PresetName = TEXT("Default");
         SkinTone = EChar_SkinTone::Medium;
-        BodyBuild = EChar_BodyBuild::Athletic;
-        TribalMarkings = EChar_TribalMarkings::None;
-        ScarIntensity = 0.3f;
-        MuscleDefinition = 0.5f;
-        bHasFacialHair = false;
+        HairStyle = EChar_HairStyle::Long;
+        ClothingStyle = EChar_ClothingStyle::Leather;
+        FacePaintColor = FLinearColor::Red;
     }
 };
 
@@ -91,30 +80,16 @@ public:
 protected:
     virtual void BeginPlay() override;
 
-    // Current visual configuration
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Character Visual", meta = (AllowPrivateAccess = "true"))
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Character Visual")
     FChar_VisualPreset CurrentPreset;
 
-    // Available presets
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Character Visual")
     TArray<FChar_VisualPreset> AvailablePresets;
 
-    // MetaHuman integration
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MetaHuman")
-    bool bUseMetaHuman;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MetaHuman")
-    TSoftObjectPtr<USkeletalMesh> MetaHumanMesh;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MetaHuman")
-    TArray<TSoftObjectPtr<UMaterialInterface>> MetaHumanMaterials;
-
-    // Character mesh reference
-    UPROPERTY(BlueprintReadOnly, Category = "Character Visual")
-    class USkeletalMeshComponent* CharacterMeshComponent;
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Character Visual")
+    class USkeletalMeshComponent* CharacterMesh;
 
 public:
-    // Visual customization methods
     UFUNCTION(BlueprintCallable, Category = "Character Visual")
     void ApplyVisualPreset(const FChar_VisualPreset& Preset);
 
@@ -122,68 +97,22 @@ public:
     void SetSkinTone(EChar_SkinTone NewSkinTone);
 
     UFUNCTION(BlueprintCallable, Category = "Character Visual")
-    void SetBodyBuild(EChar_BodyBuild NewBodyBuild);
+    void SetHairStyle(EChar_HairStyle NewHairStyle);
 
     UFUNCTION(BlueprintCallable, Category = "Character Visual")
-    void SetTribalMarkings(EChar_TribalMarkings NewMarkings);
+    void SetClothingStyle(EChar_ClothingStyle NewClothingStyle);
 
     UFUNCTION(BlueprintCallable, Category = "Character Visual")
-    void SetScarIntensity(float Intensity);
+    void SetFacePaintColor(FLinearColor NewColor);
 
-    UFUNCTION(BlueprintCallable, Category = "Character Visual")
-    void SetMuscleDefinition(float Definition);
-
-    UFUNCTION(BlueprintCallable, Category = "Character Visual")
-    void SetFacialHair(bool bHasFacialHair);
-
-    // MetaHuman methods
-    UFUNCTION(BlueprintCallable, Category = "MetaHuman")
-    void EnableMetaHuman(USkeletalMesh* MetaHumanMesh);
-
-    UFUNCTION(BlueprintCallable, Category = "MetaHuman")
-    void DisableMetaHuman();
-
-    UFUNCTION(BlueprintCallable, Category = "MetaHuman")
-    bool IsUsingMetaHuman() const { return bUseMetaHuman; }
-
-    // Preset management
-    UFUNCTION(BlueprintCallable, Category = "Character Visual")
-    void LoadPreset(const FString& PresetName);
-
-    UFUNCTION(BlueprintCallable, Category = "Character Visual")
-    void SaveCurrentAsPreset(const FString& PresetName);
-
-    UFUNCTION(BlueprintCallable, Category = "Character Visual")
-    TArray<FString> GetAvailablePresetNames() const;
-
-    // Getters
     UFUNCTION(BlueprintPure, Category = "Character Visual")
     FChar_VisualPreset GetCurrentPreset() const { return CurrentPreset; }
 
-    UFUNCTION(BlueprintPure, Category = "Character Visual")
-    EChar_SkinTone GetSkinTone() const { return CurrentPreset.SkinTone; }
-
-    UFUNCTION(BlueprintPure, Category = "Character Visual")
-    EChar_BodyBuild GetBodyBuild() const { return CurrentPreset.BodyBuild; }
-
-    UFUNCTION(BlueprintPure, Category = "Character Visual")
-    EChar_TribalMarkings GetTribalMarkings() const { return CurrentPreset.TribalMarkings; }
+    UFUNCTION(BlueprintCallable, Category = "Character Visual")
+    void InitializeDefaultPresets();
 
 private:
-    // Internal methods
-    void InitializePresets();
-    void UpdateCharacterMesh();
-    void UpdateMaterials();
-    void ApplyTribalMarkings();
-    void ApplyBodyModifications();
-
-    // Editor-only methods
-#if WITH_EDITOR
-public:
-    UFUNCTION(CallInEditor)
-    void RefreshCharacterVisuals();
-
-    UFUNCTION(CallInEditor)
-    void ResetToDefaultPreset();
-#endif
+    void UpdateCharacterAppearance();
+    void LoadCharacterMesh(TSoftObjectPtr<USkeletalMesh> MeshAsset);
+    void ApplyMaterialParameters();
 };
