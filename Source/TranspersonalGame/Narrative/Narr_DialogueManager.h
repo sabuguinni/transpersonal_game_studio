4eceed5af1afcc3e38c27c81a4f498bb232bdf1f
sabuguinni <hugomartins@transpersonalinternational.com>
2026-06-13 00:14:23 +0100

    Add Narr_DialogueManager - comprehensive dialogue system with quest integration and NPC personality support

--- Source/TranspersonalGame/Narrative/Narr_DialogueManager.h
+++ Source/TranspersonalGame/Narrative/Narr_DialogueManager.h
@@ -1,1 +1,146 @@
-undefined
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/SphereComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Engine/DataTable.h"
+#include "SharedTypes.h"
+#include "Narr_DialogueManager.generated.h"
+
+USTRUCT(BlueprintType)
+struct TRANSPERSONALGAME_API FNarr_DialogueEntry
+{
+    GENERATED_BODY()
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")
+    FString SpeakerName;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")
+    FText DialogueText;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")
+    TArray<FText> ResponseOptions;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")
+    TArray<int32> NextDialogueIDs;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")
+    ENarr_QuestStage RequiredQuestStage;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")
+    bool bIsQuestDialogue;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")
+    float EmotionalIntensity;
+
+    FNarr_DialogueEntry()
+    {
+        SpeakerName = TEXT("Unknown");
+        DialogueText = FText::GetEmpty();
+        RequiredQuestStage = ENarr_QuestStage::Tutorial;
+        bIsQuestDialogue = false;
+        EmotionalIntensity = 0.5f;
+    }
+};
+
+USTRUCT(BlueprintType)
+struct TRANSPERSONALGAME_API FNarr_DialogueTree
+{
+    GENERATED_BODY()
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")
+    TArray<FNarr_DialogueEntry> DialogueEntries;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")
+    int32 CurrentDialogueID;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")
+    FString TreeName;
+
+    FNarr_DialogueTree()
+    {
+        CurrentDialogueID = 0;
+        TreeName = TEXT("DefaultTree");
+    }
+};
+
+UCLASS(BlueprintType, Blueprintable)
+class TRANSPERSONALGAME_API ANarr_DialogueManager : public AActor
+{
+    GENERATED_BODY()
+
+public:
+    ANarr_DialogueManager();
+
+protected:
+    virtual void BeginPlay() override;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+    USphereComponent* InteractionSphere;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+    UStaticMeshComponent* VisualMarker;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")
+    FNarr_DialogueTree DialogueTree;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")
+    ENarr_NPCPersonality NPCPersonality;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")
+    float InteractionRadius;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")
+    bool bIsActive;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Audio")
+    TArray<FString> VoiceLineURLs;
+
+public:
+    virtual void Tick(float DeltaTime) override;
+
+    UFUNCTION(BlueprintCallable, Category = "Dialogue")
+    void StartDialogue(AActor* PlayerActor);
+
+    UFUNCTION(BlueprintCallable, Category = "Dialogue")
+    void EndDialogue();
+
+    UFUNCTION(BlueprintCallable, Category = "Dialogue")
+    FNarr_DialogueEntry GetCurrentDialogue();
+
+    UFUNCTION(BlueprintCallable, Category = "Dialogue")
+    void SelectResponse(int32 ResponseIndex);
+
+    UFUNCTION(BlueprintCallable, Category = "Dialogue")
+    bool CanStartDialogue(AActor* PlayerActor);
+
+    UFUNCTION(BlueprintCallable, Category = "Dialogue")
+    void SetDialogueTree(const FNarr_DialogueTree& NewTree);
+
+    UFUNCTION(BlueprintCallable, Category = "Dialogue")
+    void AddVoiceLine(const FString& VoiceURL);
+
+    UFUNCTION(BlueprintImplementableEvent, Category = "Dialogue")
+    void OnDialogueStarted();
+
+    UFUNCTION(BlueprintImplementableEvent, Category = "Dialogue")
+    void OnDialogueEnded();
+
+    UFUNCTION(BlueprintImplementableEvent, Category = "Dialogue")
+    void OnResponseSelected(int32 ResponseIndex);
+
+protected:
+    UFUNCTION()
+    void OnInteractionSphereBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+    UFUNCTION()
+    void OnInteractionSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+private:
+    bool bPlayerInRange;
+    AActor* CurrentPlayer;
+    
+    void UpdateVisualMarker();
+    bool CheckQuestRequirements(const FNarr_DialogueEntry& Entry);
+};
