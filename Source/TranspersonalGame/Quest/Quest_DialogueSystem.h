0d79670309397d7d3d7d9e807e31ed541c04c8cf
sabuguinni <hugomartins@transpersonalinternational.com>
2026-06-13 12:08:53 +0100

    Add Quest_DialogueSystem component for NPC conversations and quest interactions

--- Source/TranspersonalGame/Quest/Quest_DialogueSystem.h
+++ Source/TranspersonalGame/Quest/Quest_DialogueSystem.h
@@ -1,9 +1,22 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Engine/GameInstance.h"
-#include "Subsystems/GameInstanceSubsystem.h"
+#include "Components/ActorComponent.h"
+#include "Engine/DataTable.h"
 #include "Quest_DialogueSystem.generated.h"
+
+UENUM(BlueprintType)
+enum class EQuest_DialogueType : uint8
+{
+    QuestGive       UMETA(DisplayName = "Quest Give"),
+    QuestComplete   UMETA(DisplayName = "Quest Complete"),
+    QuestProgress   UMETA(DisplayName = "Quest Progress"),
+    Greeting        UMETA(DisplayName = "Greeting"),
+    Trade           UMETA(DisplayName = "Trade"),
+    Information     UMETA(DisplayName = "Information"),
+    Warning         UMETA(DisplayName = "Warning"),
+    Farewell        UMETA(DisplayName = "Farewell")
+};
 
 USTRUCT(BlueprintType)
 struct TRANSPERSONALGAME_API FQuest_DialogueLine
@@ -14,97 +27,109 @@ struct TRANSPERSONALGAME_API FQuest_DialogueLine
     FString SpeakerName;
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")
-    FString DialogueText;
+    FText DialogueText;
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")
-    FString AudioURL;
+    EQuest_DialogueType DialogueType;
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")
-    float Duration;
+    FString AudioPath;
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")
-    bool bIsQuestRelated;
+    float DisplayDuration;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")
+    TArray<FString> PlayerResponses;
 
     FQuest_DialogueLine()
     {
-        SpeakerName = TEXT("");
-        DialogueText = TEXT("");
-        AudioURL = TEXT("");
-        Duration = 3.0f;
-        bIsQuestRelated = false;
+        SpeakerName = TEXT("Unknown");
+        DialogueText = FText::FromString(TEXT("..."));
+        DialogueType = EQuest_DialogueType::Information;
+        AudioPath = TEXT("");
+        DisplayDuration = 3.0f;
     }
 };
 
 USTRUCT(BlueprintType)
-struct TRANSPERSONALGAME_API FQuest_DialogueTree
+struct TRANSPERSONALGAME_API FQuest_DialogueSequence
 {
     GENERATED_BODY()
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")
-    FString TreeID;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")
-    FString NPCName;
+    FString SequenceID;
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")
     TArray<FQuest_DialogueLine> DialogueLines;
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")
-    TArray<FString> RequiredQuestIDs;
+    FString NextSequenceID;
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue")
     bool bIsRepeatable;
 
-    FQuest_DialogueTree()
+    FQuest_DialogueSequence()
     {
-        TreeID = TEXT("");
-        NPCName = TEXT("");
-        bIsRepeatable = true;
+        SequenceID = TEXT("default");
+        NextSequenceID = TEXT("");
+        bIsRepeatable = false;
     }
 };
 
-UCLASS(BlueprintType, Blueprintable)
-class TRANSPERSONALGAME_API UQuest_DialogueSystem : public UGameInstanceSubsystem
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class TRANSPERSONALGAME_API UQuest_DialogueSystem : public UActorComponent
 {
     GENERATED_BODY()
 
 public:
     UQuest_DialogueSystem();
 
-    virtual void Initialize(FSubsystemCollectionBase& Collection) override;
-
-    UFUNCTION(BlueprintCallable, Category = "Quest Dialogue")
-    void RegisterDialogueTree(const FQuest_DialogueTree& DialogueTree);
-
-    UFUNCTION(BlueprintCallable, Category = "Quest Dialogue")
-    FQuest_DialogueTree GetDialogueTreeForNPC(const FString& NPCName);
-
-    UFUNCTION(BlueprintCallable, Category = "Quest Dialogue")
-    bool StartDialogue(const FString& NPCName, AActor* PlayerActor);
-
-    UFUNCTION(BlueprintCallable, Category = "Quest Dialogue")
-    void PlayDialogueLine(const FQuest_DialogueLine& DialogueLine);
-
-    UFUNCTION(BlueprintCallable, Category = "Quest Dialogue")
-    void EndDialogue();
-
-    UFUNCTION(BlueprintCallable, Category = "Quest Dialogue")
-    bool IsDialogueActive() const;
-
 protected:
-    UPROPERTY(BlueprintReadOnly, Category = "Quest Dialogue")
-    TMap<FString, FQuest_DialogueTree> DialogueTrees;
+    virtual void BeginPlay() override;
 
-    UPROPERTY(BlueprintReadOnly, Category = "Quest Dialogue")
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue System")
+    TMap<FString, FQuest_DialogueSequence> DialogueSequences;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue System")
+    FString CurrentSequenceID;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue System")
+    int32 CurrentLineIndex;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue System")
     bool bIsDialogueActive;
 
-    UPROPERTY(BlueprintReadOnly, Category = "Quest Dialogue")
-    FString CurrentNPCName;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dialogue System")
+    float InteractionRange;
 
-    UPROPERTY(BlueprintReadOnly, Category = "Quest Dialogue")
-    int32 CurrentLineIndex;
+public:
+    UFUNCTION(BlueprintCallable, Category = "Dialogue System")
+    bool StartDialogue(const FString& SequenceID);
+
+    UFUNCTION(BlueprintCallable, Category = "Dialogue System")
+    void NextDialogueLine();
+
+    UFUNCTION(BlueprintCallable, Category = "Dialogue System")
+    void EndDialogue();
+
+    UFUNCTION(BlueprintCallable, Category = "Dialogue System")
+    FQuest_DialogueLine GetCurrentDialogueLine() const;
+
+    UFUNCTION(BlueprintCallable, Category = "Dialogue System")
+    bool IsDialogueActive() const { return bIsDialogueActive; }
+
+    UFUNCTION(BlueprintCallable, Category = "Dialogue System")
+    void AddDialogueSequence(const FString& SequenceID, const FQuest_DialogueSequence& Sequence);
+
+    UFUNCTION(BlueprintCallable, Category = "Dialogue System")
+    void SetupHuntQuestDialogue();
+
+    UFUNCTION(BlueprintCallable, Category = "Dialogue System")
+    void SetupCollectQuestDialogue();
+
+    UFUNCTION(BlueprintCallable, Category = "Dialogue System")
+    void SetupExploreQuestDialogue();
 
 private:
     void InitializeDefaultDialogues();
-    bool CanAccessDialogue(const FQuest_DialogueTree& DialogueTree);
 };
