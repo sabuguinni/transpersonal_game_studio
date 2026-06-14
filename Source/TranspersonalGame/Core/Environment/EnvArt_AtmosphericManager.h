a9bba887928a678bef03bd04443a37e8803f1073
sabuguinni <hugomartins@transpersonalinternational.com>
2026-06-14 06:14:38 +0100

    Add EnvArt_AtmosphericManager for dynamic lighting, fog, and atmospheric effects

--- Source/TranspersonalGame/Core/Environment/EnvArt_AtmosphericManager.h
+++ Source/TranspersonalGame/Core/Environment/EnvArt_AtmosphericManager.h
@@ -3,80 +3,47 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "Components/StaticMeshComponent.h"
-#include "Components/AudioComponent.h"
-#include "Particles/ParticleSystemComponent.h"
+#include "Components/SceneComponent.h"
 #include "Engine/DirectionalLight.h"
-#include "Engine/VolumetricCloud.h"
-#include "Engine/TriggerBox.h"
+#include "Components/DirectionalLightComponent.h"
+#include "Components/VolumetricCloudComponent.h"
+#include "Components/AudioComponent.h"
+#include "Sound/SoundCue.h"
+#include "Particles/ParticleSystemComponent.h"
 #include "SharedTypes.h"
 #include "EnvArt_AtmosphericManager.generated.h"
 
 USTRUCT(BlueprintType)
-struct FEnvArt_AtmosphericZone
+struct TRANSPERSONALGAME_API FEnvArt_AtmosphericSettings
 {
     GENERATED_BODY()
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Atmosphere")
-    FString ZoneName;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Lighting")
+    FLinearColor SunColor = FLinearColor(1.0f, 0.8f, 0.6f, 1.0f);
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Atmosphere")
-    FVector ZoneCenter;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Lighting")
+    float SunIntensity = 3.5f;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Atmosphere")
-    float ZoneRadius;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Lighting")
+    float SunElevation = -30.0f;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Atmosphere")
-    FLinearColor FogColor;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Lighting")
+    float SunAzimuth = 45.0f;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Atmosphere")
-    float FogDensity;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Fog")
+    float FogDensity = 0.1f;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Atmosphere")
-    FLinearColor LightColor;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Fog")
+    float FogHeight = 200.0f;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Atmosphere")
-    float LightIntensity;
-
-    FEnvArt_AtmosphericZone()
-    {
-        ZoneName = TEXT("DefaultZone");
-        ZoneCenter = FVector::ZeroVector;
-        ZoneRadius = 5000.0f;
-        FogColor = FLinearColor(0.9f, 0.95f, 1.0f, 1.0f);
-        FogDensity = 0.1f;
-        LightColor = FLinearColor(1.0f, 0.9f, 0.7f, 1.0f);
-        LightIntensity = 8.0f;
-    }
-};
-
-USTRUCT(BlueprintType)
-struct FEnvArt_ParticleEffect
-{
-    GENERATED_BODY()
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Fog")
+    FLinearColor FogColor = FLinearColor(0.9f, 0.95f, 1.0f, 1.0f);
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Particles")
-    FString EffectName;
+    float ParticleDensity = 50.0f;
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Particles")
-    FVector SpawnLocation;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Particles")
-    float SpawnRate;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Particles")
-    float ParticleLifetime;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Particles")
-    bool bIsActive;
-
-    FEnvArt_ParticleEffect()
-    {
-        EffectName = TEXT("DefaultParticles");
-        SpawnLocation = FVector::ZeroVector;
-        SpawnRate = 10.0f;
-        ParticleLifetime = 5.0f;
-        bIsActive = true;
-    }
+    float WindStrength = 1.0f;
 };
 
 UCLASS(BlueprintType, Blueprintable)
@@ -93,68 +60,63 @@ protected:
 public:
     virtual void Tick(float DeltaTime) override;
 
-    // Atmospheric zones management
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Atmospheric Zones")
-    TArray<FEnvArt_AtmosphericZone> AtmosphericZones;
-
-    // Particle effects management
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Particle Effects")
-    TArray<FEnvArt_ParticleEffect> ParticleEffects;
-
-    // Time of day settings
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Time of Day", meta = (ClampMin = "0.0", ClampMax = "24.0"))
-    float CurrentTimeOfDay;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Time of Day")
-    float DayDurationInMinutes;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Time of Day")
-    bool bEnableDayNightCycle;
-
-    // Weather settings
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weather")
-    float WeatherIntensity;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weather")
-    bool bIsRaining;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weather")
-    bool bIsFoggy;
-
-    // Component references
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
-    class USceneComponent* RootSceneComponent;
+    USceneComponent* RootSceneComponent;
 
-    // Atmospheric functions
-    UFUNCTION(BlueprintCallable, Category = "Atmosphere")
-    void SetAtmosphericZone(const FString& ZoneName, const FVector& PlayerLocation);
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Atmospheric Settings")
+    FEnvArt_AtmosphericSettings AtmosphericSettings;
 
-    UFUNCTION(BlueprintCallable, Category = "Atmosphere")
-    void UpdateLightingForTimeOfDay(float TimeOfDay);
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "References")
+    ADirectionalLight* SunLight;
 
-    UFUNCTION(BlueprintCallable, Category = "Atmosphere")
-    void SpawnParticleEffect(const FString& EffectName, const FVector& Location);
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "References")
+    AActor* VolumetricFogActor;
 
-    UFUNCTION(BlueprintCallable, Category = "Atmosphere")
-    void SetWeatherConditions(bool bRain, bool bFog, float Intensity);
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+    UParticleSystemComponent* AtmosphericParticles;
 
-    UFUNCTION(BlueprintCallable, Category = "Atmosphere")
-    FEnvArt_AtmosphericZone GetCurrentAtmosphericZone(const FVector& Location) const;
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+    UAudioComponent* AmbientAudioComponent;
 
-    UFUNCTION(BlueprintCallable, Category = "Atmosphere")
-    void InitializeDefaultAtmosphericZones();
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Audio")
+    USoundCue* ForestAmbientSound;
 
-    UFUNCTION(BlueprintCallable, Category = "Atmosphere")
-    void UpdateAtmosphericEffects(float DeltaTime);
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Audio")
+    USoundCue* WindSound;
+
+    UFUNCTION(BlueprintCallable, Category = "Atmospheric Control")
+    void SetTimeOfDay(float TimeHours);
+
+    UFUNCTION(BlueprintCallable, Category = "Atmospheric Control")
+    void SetWeatherIntensity(float Intensity);
+
+    UFUNCTION(BlueprintCallable, Category = "Atmospheric Control")
+    void SetFogDensity(float Density);
+
+    UFUNCTION(BlueprintCallable, Category = "Atmospheric Control")
+    void SetWindStrength(float Strength);
+
+    UFUNCTION(BlueprintCallable, Category = "Atmospheric Control")
+    void ApplyGoldenHourLighting();
+
+    UFUNCTION(BlueprintCallable, Category = "Atmospheric Control")
+    void ApplyMidnightLighting();
+
+    UFUNCTION(BlueprintCallable, Category = "Atmospheric Control")
+    void CreateVolumetricFogZone(FVector Location, FVector Scale);
+
+    UFUNCTION(BlueprintCallable, Category = "Atmospheric Control")
+    void SpawnAtmosphericParticles(FVector Location, float Density);
 
 private:
-    // Internal state
-    FString CurrentZoneName;
-    float TimeAccumulator;
-    
-    // Helper functions
-    void UpdateDirectionalLight();
-    void UpdateVolumetricFog();
+    UPROPERTY()
+    float CurrentTimeOfDay = 12.0f;
+
+    UPROPERTY()
+    float CurrentWeatherIntensity = 0.5f;
+
+    void UpdateLightingBasedOnTime();
+    void UpdateFogBasedOnWeather();
     void UpdateParticleEffects();
-    void CalculateAtmosphericBlending(const FVector& PlayerLocation);
+    void UpdateAmbientAudio();
 };
