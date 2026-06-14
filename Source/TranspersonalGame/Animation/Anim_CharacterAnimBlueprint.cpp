6acd9c7271706a8f2a7f270358ca172b1ca6849a
sabuguinni <hugomartins@transpersonalinternational.com>
2026-06-14 09:58:48 +0100

    Implement character animation blueprint logic with movement state detection

--- Source/TranspersonalGame/Animation/Anim_CharacterAnimBlueprint.cpp
+++ Source/TranspersonalGame/Animation/Anim_CharacterAnimBlueprint.cpp
@@ -5,156 +5,107 @@
 
 UAnim_CharacterAnimBlueprint::UAnim_CharacterAnimBlueprint()
 {
-    OwnerCharacter = nullptr;
+    OwningCharacter = nullptr;
     MovementComponent = nullptr;
-    WalkSpeedThreshold = 150.0f;
+    WalkSpeedThreshold = 100.0f;
     RunSpeedThreshold = 300.0f;
-    JumpVelocityThreshold = 100.0f;
-    FallVelocityThreshold = -100.0f;
+    JumpVelocityThreshold = 50.0f;
 }
 
 void UAnim_CharacterAnimBlueprint::NativeInitializeAnimation()
 {
     Super::NativeInitializeAnimation();
-    UpdateCharacterReferences();
+
+    OwningCharacter = Cast<ACharacter>(GetOwningActor());
+    if (OwningCharacter)
+    {
+        MovementComponent = OwningCharacter->GetCharacterMovement();
+    }
 }
 
 void UAnim_CharacterAnimBlueprint::NativeUpdateAnimation(float DeltaTimeX)
 {
     Super::NativeUpdateAnimation(DeltaTimeX);
-    
-    if (!OwnerCharacter || !MovementComponent)
-    {
-        UpdateCharacterReferences();
-        return;
-    }
 
-    UpdateMovementData();
-    UpdateMovementState();
-    CalculateDirection();
-}
-
-void UAnim_CharacterAnimBlueprint::UpdateCharacterReferences()
-{
-    OwnerCharacter = Cast<ACharacter>(TryGetPawnOwner());
-    if (OwnerCharacter)
-    {
-        MovementComponent = OwnerCharacter->GetCharacterMovement();
-    }
-}
-
-void UAnim_CharacterAnimBlueprint::UpdateMovementData()
-{
-    if (!OwnerCharacter || !MovementComponent)
+    if (!OwningCharacter || !MovementComponent)
     {
         return;
     }
 
-    // Update basic movement data
+    // Update movement data
     FVector Velocity = MovementComponent->Velocity;
-    MovementData.Speed = Velocity.Size2D();
+    MovementData.Speed = Velocity.Size();
+    MovementData.Direction = UKismetMathLibrary::CalculateDirection(Velocity, OwningCharacter->GetActorRotation()).Yaw;
     MovementData.bIsInAir = MovementComponent->IsFalling();
     MovementData.bIsCrouching = MovementComponent->IsCrouching();
     MovementData.bIsSwimming = MovementComponent->IsSwimming();
+
+    UpdateMovementState();
 }
 
 void UAnim_CharacterAnimBlueprint::UpdateMovementState()
 {
-    if (!OwnerCharacter || !MovementComponent)
-    {
-        MovementData.MovementState = EAnim_MovementState::Idle;
-        return;
-    }
-
-    // Priority order: Air states -> Ground states
-    if (ShouldEnterJumpState())
-    {
-        MovementData.MovementState = EAnim_MovementState::Jumping;
-    }
-    else if (ShouldEnterFallState())
-    {
-        MovementData.MovementState = EAnim_MovementState::Falling;
-    }
-    else if (MovementData.bIsSwimming)
-    {
-        MovementData.MovementState = EAnim_MovementState::Swimming;
-    }
-    else if (MovementData.bIsCrouching)
-    {
-        MovementData.MovementState = EAnim_MovementState::Crouching;
-    }
-    else if (ShouldEnterRunState())
-    {
-        MovementData.MovementState = EAnim_MovementState::Running;
-    }
-    else if (ShouldEnterWalkState())
-    {
-        MovementData.MovementState = EAnim_MovementState::Walking;
-    }
-    else
-    {
-        MovementData.MovementState = EAnim_MovementState::Idle;
-    }
+    MovementData.MovementState = CalculateMovementState();
 }
 
-void UAnim_CharacterAnimBlueprint::CalculateDirection()
-{
-    if (!OwnerCharacter || !MovementComponent)
-    {
-        MovementData.Direction = 0.0f;
-        return;
-    }
-
-    FVector Velocity = MovementComponent->Velocity;
-    FVector ForwardVector = OwnerCharacter->GetActorForwardVector();
-    
-    if (Velocity.Size2D() > 0.1f)
-    {
-        FVector NormalizedVelocity = Velocity.GetSafeNormal2D();
-        float DotProduct = FVector::DotProduct(ForwardVector, NormalizedVelocity);
-        float CrossProduct = FVector::CrossProduct(ForwardVector, NormalizedVelocity).Z;
-        
-        MovementData.Direction = UKismetMathLibrary::DegAtan2(CrossProduct, DotProduct);
-    }
-    else
-    {
-        MovementData.Direction = 0.0f;
-    }
-}
-
-bool UAnim_CharacterAnimBlueprint::ShouldEnterIdleState() const
-{
-    return MovementData.Speed < WalkSpeedThreshold && !MovementData.bIsInAir;
-}
-
-bool UAnim_CharacterAnimBlueprint::ShouldEnterWalkState() const
-{
-    return MovementData.Speed >= WalkSpeedThreshold && 
-           MovementData.Speed < RunSpeedThreshold && 
-           !MovementData.bIsInAir;
-}
-
-bool UAnim_CharacterAnimBlueprint::ShouldEnterRunState() const
-{
-    return MovementData.Speed >= RunSpeedThreshold && !MovementData.bIsInAir;
-}
-
-bool UAnim_CharacterAnimBlueprint::ShouldEnterJumpState() const
+EAnim_MovementState UAnim_CharacterAnimBlueprint::CalculateMovementState() const
 {
     if (!MovementComponent)
     {
-        return false;
+        return EAnim_MovementState::Idle;
     }
-    
-    return MovementData.bIsInAir && MovementComponent->Velocity.Z > JumpVelocityThreshold;
+
+    if (MovementData.bIsSwimming)
+    {
+        return EAnim_MovementState::Swimming;
+    }
+
+    if (MovementData.bIsInAir)
+    {
+        if (MovementComponent->Velocity.Z > JumpVelocityThreshold)
+        {
+            return EAnim_MovementState::Jumping;
+        }
+        else
+        {
+            return EAnim_MovementState::Falling;
+        }
+    }
+
+    if (MovementData.bIsCrouching)
+    {
+        return EAnim_MovementState::Crouching;
+    }
+
+    if (MovementData.Speed > RunSpeedThreshold)
+    {
+        return EAnim_MovementState::Running;
+    }
+    else if (MovementData.Speed > WalkSpeedThreshold)
+    {
+        return EAnim_MovementState::Walking;
+    }
+
+    return EAnim_MovementState::Idle;
 }
 
-bool UAnim_CharacterAnimBlueprint::ShouldEnterFallState() const
+bool UAnim_CharacterAnimBlueprint::ShouldPlayIdleAnimation() const
 {
-    if (!MovementComponent)
-    {
-        return false;
-    }
-    
-    return MovementData.bIsInAir && MovementComponent->Velocity.Z < FallVelocityThreshold;
+    return MovementData.MovementState == EAnim_MovementState::Idle;
+}
+
+bool UAnim_CharacterAnimBlueprint::ShouldPlayWalkAnimation() const
+{
+    return MovementData.MovementState == EAnim_MovementState::Walking;
+}
+
+bool UAnim_CharacterAnimBlueprint::ShouldPlayRunAnimation() const
+{
+    return MovementData.MovementState == EAnim_MovementState::Running;
+}
+
+bool UAnim_CharacterAnimBlueprint::ShouldPlayJumpAnimation() const
+{
+    return MovementData.MovementState == EAnim_MovementState::Jumping || 
+           MovementData.MovementState == EAnim_MovementState::Falling;
 }
